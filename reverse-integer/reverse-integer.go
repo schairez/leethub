55189077307404f2010e283e78b615446817ce3d
Sergio Chairez <schairezv@gmail.com>
2021-09-14 06:01:10 -0700

    Time: 8 ms (5.20%), Space: 2.4 MB (13.66%) - LeetHub

---
+++ reverse-integer/reverse-integer.go
@@ -0,0 +1,39 @@
+
+/*
+time: O(n)
+space: O(1)
+*/
+
+func checkValidI32(x int) bool {
+    minInt32 := -1 * (1 <<31) 
+    maxInt32 := (1 << 31) - 1
+    if x < minInt32 || x > maxInt32 {
+        return false
+    }
+    return true 
+}
+
+func reverse(x int) int {
+    reverse_x := 0
+    isNegative := false 
+    if x < 0 {
+        isNegative = true 
+        x = -1 * x
+    }
+    for x > 0 {
+        reverse_x = reverse_x * 10 + x % 10 
+        x = x / 10
+    }
+    if !checkValidI32(reverse_x) {
+        return 0
+    }
+    if isNegative {
+        return -1 * reverse_x
+    }
+    return reverse_x 
+    
+    
+    
+    
+    
+}
