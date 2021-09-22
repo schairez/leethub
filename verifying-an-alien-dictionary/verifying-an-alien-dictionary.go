58c0f52b636d458a1d2834915828464976c29855
Sergio Chairez <schairezv@gmail.com>
2021-09-22 15:30:11 -0700

    Time: 0 ms (100.00%), Space: 2.7 MB (93.23%) - LeetHub

---
+++ verifying-an-alien-dictionary/verifying-an-alien-dictionary.go
@@ -0,0 +1,65 @@
+/*
+time: O(n^2)
+space: O(n)
+*/
+
+import "fmt"
+
+func isAlienSorted(words []string, order string) bool {
+    alienCodes := newAlienCodes(order)
+    isSorted := true 
+    lPtr, rPtr := 0, 1 
+    for rPtr != len(words) && isSorted {
+        wordL, wordR := words[lPtr], words[rPtr]
+        if isLess := alienCodes.less(wordL,wordR); !isLess {
+            fmt.Println(wordL, wordR)
+            return false
+        }
+        lPtr++
+        rPtr++
+    }
+    return isSorted 
+    
+    
+}
+type alienCodes struct {
+    codes map[byte]int
+}
+
+func newAlienCodes(order string) alienCodes {
+    lexMap := make(map[byte]int, 26)
+    //representing the order of the byte in order string
+    for i:=0; i < len(order); i++ {
+        var b byte = order[i]
+        lexMap[b] = i
+    }
+    alien := alienCodes{codes: lexMap}
+    return alien
+}
+
+func (a alienCodes) less(left, right string) bool {
+    isSorted := true 
+    //we iter length of smallest of two inputs 
+    lenL, lenR := len(left), len(right)
+    smallestLen := lenL
+    if lenL > lenR {
+        smallestLen = lenR
+    }
+    for i:=0; i < smallestLen; i++ {
+        byteL, byteR := left[i], right[i]
+        codeL, codeR := a.codes[byteL], a.codes[byteR]
+        if codeL == codeR {
+            continue
+        } else if codeL > codeR {
+            return false
+        } else {
+            return true 
+        }
+        
+    }
+    //in the case that len(l) > len(r); any char in l > the blank identifier '∅'
+    if lenL > lenR {
+        isSorted = false
+    }
+    return isSorted
+}
