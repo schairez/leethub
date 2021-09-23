d583a5dd26a656bf238ac638e1978fafa428aabb
Sergio Chairez <schairezv@gmail.com>
2021-09-22 17:21:58 -0700

    Time: 12 ms (88.33%), Space: 5.7 MB (51.00%) - LeetHub

---
+++ top-k-frequent-elements/top-k-frequent-elements.go
@@ -0,0 +1,42 @@
+/*
+
+*/
+
+import "fmt"
+
+func topKFrequent(nums []int, k int) []int {
+    if len(nums) == 1 {
+        return nums
+    }
+    res := make([]int, 0, k)
+    freqMap := make(map[int]int)
+    for _, num := range nums {
+        if _, ok := freqMap[num]; !ok {
+            freqMap[num] = 0 
+        }
+        freqMap[num]++
+    }
+    //inverse frequency
+    invFreq := map[int][]int{}
+    for k,v := range freqMap {
+        if _, ok := invFreq[v]; !ok {
+            invFreq[v] = []int{}
+        } 
+        invFreq[v] = append(invFreq[v], k)
+    }
+    fmt.Println(invFreq)
+    for i:=len(nums); i >=0; i-- {
+        if len(res) == k {
+            break
+        }
+        if freqElemsArr, ok := invFreq[i]; ok {
+            res = append(res, freqElemsArr...)
+        }
+    }
+    
+    
+    
+    return res
+    
+}
+
