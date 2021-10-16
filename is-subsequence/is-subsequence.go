a474ee628a57e0ee3c5506bd127f11e7f7aa0d46
Sergio Chairez <schairezv@gmail.com>
2021-10-16 06:28:02 -0700

    Time: 0 ms (100.00%), Space: 2.2 MB (32.26%) - LeetHub

--- is-subsequence/is-subsequence.go
+++ is-subsequence/is-subsequence.go
@@ -1,9 +1,24 @@
+//iterative
+// time: O(len(t))
+// space: O(1)
+func isSubsequence(s string, t string) bool {
+    //if subsequence; num of common valid chars == len(s) 
+    lenS := len(s)
+    lenT := len(t)
+    lenCommon := 0
+    for i:=0; i < lenT && lenCommon < lenS; i++ {
+        if t[i] == s[lenCommon] {
+            lenCommon++
+        }     
+    }
+    return lenS == lenCommon
+    
+}
 /*
-time: O(len(t)) //worst-case we iterate through all bytes of larger string
-space: O(n) //stack space
-*/
+//time: O(len(t)) //worst-case we iterate through all bytes of larger string
+//space: O(n) //stack space
 
-//is s subsequence of t
+//is s subsequence of t; recursive
 func isSubsequence(s string, t string) bool {
     if len(s) == 0 { return true }
     if len(t) == 0 { return false }
@@ -14,3 +29,5 @@ func isSubsequence(s string, t string) bool {
     return isSubsequence(s, t[1:])
     
 }
+
+*/
