e76f8e14ff59a0caec228bf7e736d305b287bdf2
Sergio Chairez <schairezv@gmail.com>
2021-10-17 02:25:51 -0700

    Time: 52 ms (93.32%), Space: 18.1 MB (32.06%) - LeetHub

---
+++ implement-trie-prefix-tree/implement-trie-prefix-tree.go
@@ -0,0 +1,62 @@
+
+//consists of only english letters so [26]*Trie 
+
+type Trie struct {
+    children [26]*Trie
+    isLeaf bool
+    
+}
+
+
+func Constructor() Trie {
+    return Trie{}
+}
+
+
+func (this *Trie) Insert(word string)  {
+    curr := this
+    for i:=0; i < len(word); i++ {
+        idx := word[i] - 'a'
+        if v := curr.children[idx]; v == nil {
+            curr.children[idx] = &Trie{}
+        }
+        curr = curr.children[idx]
+    }
+    curr.isLeaf = true
+    return
+}
+
+
+func (this *Trie) Search(word string) bool {
+    curr := this
+    for i:=0; i < len(word); i++ {
+        idx := word[i] - 'a'
+        if v := curr.children[idx]; v == nil {
+            return false
+        }
+        curr = curr.children[idx]
+    }
+    return curr.isLeaf
+}
+
+
+func (this *Trie) StartsWith(prefix string) bool {
+    curr := this
+    for i:=0; i < len(prefix); i++ {
+        idx := prefix[i] - 'a'
+        if v := curr.children[idx]; v == nil {
+            return false
+        }
+        curr = curr.children[idx]
+    }
+    return true
+}
+
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Insert(word);
+ * param_2 := obj.Search(word);
+ * param_3 := obj.StartsWith(prefix);
+ */
