2957b975f0d6cfe0fec186ec694813f9905c88ee
Sergio Chairez <schairezv@gmail.com>
2021-10-21 21:48:34 -0700

    Time: 0 ms (100.00%), Space: 2.4 MB (67.71%) - LeetHub

---
+++ construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.go
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func bstFromPreorder(preorder []int) *TreeNode {
+    //var curr *TreeNode
+    root := &TreeNode{Val: preorder[0], Left: nil, Right: nil}
+    //curr = root
+    for i:=1; i < len(preorder); i++ {
+        arrVal := preorder[i]
+        curr := root
+        newNode :=  &TreeNode{Val: arrVal, Left: nil, Right: nil} 
+        for curr != nil {
+            if arrVal < curr.Val {
+                if curr.Left == nil { 
+                    curr.Left = newNode
+                    break
+                } 
+                curr = curr.Left
+            } else {
+                if curr.Right == nil {
+                    curr.Right = newNode
+                    break
+                }
+                curr = curr.Right
+            }
+        }
+    }
+    
+    return root
+}
