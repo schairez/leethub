d25d736fc1c4d44cdcb2f7e30bc852b69ebe2b29
Sergio Chairez <schairezv@gmail.com>
2021-10-23 00:04:29 -0700

    Time: 424 ms (21.25%), Space: 6.1 MB (94.69%) - LeetHub

--- word-ladder/word-ladder.go
+++ word-ladder/word-ladder.go
@@ -11,47 +11,41 @@ type set map[string]bool
 func NewSet(words []string) set {
   s := make(set)
   for i := range words {
-    s[words[i]] = true
+      s[words[i]] = true
   }
-  return s
+    return s
 }
 
 func ladderLength(beginWord string, endWord string, wordList []string) int {
-  n := len(beginWord)
-  //curr := beginWord
-  s := NewSet(wordList)
-  //precondition
-  if _, ok := s[endWord]; !ok {
-    return 0
-  }
-  q := []*Node{}
-  q = append(q, &Node{beginWord, 1})
-  var degrees int
-  var dist int
-  for len(q) > 0 {
-    node := q[0] //hit
-    if node.word == endWord {
-      return node.dist
+    n := len(beginWord)
+    //curr := beginWord
+    s := NewSet(wordList)
+    //precondition
+    if _, ok := s[endWord]; !ok {
+        return 0
     }
-    dist = node.dist
-    curr := node.word
-    q = q[1:]
-    for k := range s { //O(n)
-      degrees = 0
-      for j := range curr {
-      if k[j] == curr[j] { //O(len(word))
-          degrees++
+    q := []*Node{}
+    q = append(q, &Node{beginWord, 1})
+    var degrees int
+    var dist int
+    for len(q) > 0 {
+        node := q[0] //hit
+        if node.word == endWord { return node.dist }
+        dist = node.dist
+        curr := node.word
+        q = q[1:]
+        for k := range s { //O(n)
+            degrees = 0
+            for j := range curr {
+                if k[j] == curr[j] { //O(len(word))
+                    degrees++
+                }
+            }
+            if degrees == n-1 {
+                q = append(q, &Node{k, dist + 1})
+                delete(s, k) //O(1)
+            }
         }
-      }
-      // hit ~ hot
-      if degrees == n-1 {
-        q = append(q, &Node{k, dist + 1})
-        //curr = s[i]
-        delete(s, k) //O(1)
-      }
     }
-  }
-
-  return 0
-
+    return 0
 }
