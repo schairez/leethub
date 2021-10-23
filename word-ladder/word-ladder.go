85c7cfc3375700b785893c5e8fc55f6c5dae2021
Sergio Chairez <schairezv@gmail.com>
2021-10-22 23:36:28 -0700

    Time: 536 ms (15.94%), Space: 6.2 MB (92.27%) - LeetHub

---
+++ word-ladder/word-ladder.go
@@ -0,0 +1,57 @@
+// time: O(N^2* len(word));
+// space: O(n)
+
+type Node struct {
+  word string
+  dist int
+}
+
+type set map[string]bool
+
+func NewSet(words []string) set {
+  s := make(set)
+  for i := range words {
+    s[words[i]] = true
+  }
+  return s
+}
+
+func ladderLength(beginWord string, endWord string, wordList []string) int {
+  n := len(beginWord)
+  //curr := beginWord
+  s := NewSet(wordList)
+  //precondition
+  if _, ok := s[endWord]; !ok {
+    return 0
+  }
+  q := []*Node{}
+  q = append(q, &Node{beginWord, 1})
+  var degrees int
+  var dist int
+  for len(q) > 0 {
+    node := q[0] //hit
+    if node.word == endWord {
+      return node.dist
+    }
+    dist = node.dist
+    curr := node.word
+    q = q[1:]
+    for k := range s { //O(n)
+      degrees = 0
+      for j := range curr {
+      if k[j] == curr[j] { //O(len(word))
+          degrees++
+        }
+      }
+      // hit ~ hot
+      if degrees == n-1 {
+        q = append(q, &Node{k, dist + 1})
+        //curr = s[i]
+        delete(s, k) //O(1)
+      }
+    }
+  }
+
+  return 0
+
+}
