8a5abe0dce89bfc9d19eba20b6fe715af98c3d3c
Sergio Chairez <schairezv@gmail.com>
2021-10-24 05:25:56 -0700

    Time: 0 ms (100.00%), Space: 2.5 MB (61.97%) - LeetHub

---
+++ find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.go
@@ -0,0 +1,29 @@
+//time:O(logN)
+//space:O(1)
+
+
+func findMin(nums []int) int {
+    var mid int
+    lo, hi := 0, len(nums)-1
+    for nums[lo] > nums[hi] {
+        mid = lo + (hi-lo) >> 1
+        if nums[mid] < nums[hi] {
+            hi = mid
+        } else {
+            lo = mid+1
+        }
+        
+    }
+    return nums[lo]
+    
+}
+
+/*
+
+
+        if nums[mid] > nums[lo] {
+            lo = mid+1
+        } else {
+            hi = mid
+        }
+*/
