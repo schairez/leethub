f6efdcb127923ad7deaf628d8709ab2b51166396
Sergio Chairez <schairezv@gmail.com>
2021-10-30 23:24:55 -0700

    Time: 20 ms (90.12%), Space: 6.3 MB (65.43%) - LeetHub

---
+++ maximum-units-on-a-truck/maximum-units-on-a-truck.go
@@ -0,0 +1,28 @@
+//time: O(nlongn + n) ~ O(nlogn)
+//space: O(1)
+
+import "sort"
+
+func maximumUnits(boxTypes [][]int, truckSize int) int {
+    sort.Slice(boxTypes, func(i, j int) bool { //time: O(nlogn) 
+        return boxTypes[i][1] > boxTypes[j][1]
+    })
+    boxesLeft := truckSize
+    totalUnits := 0
+    for i := range boxTypes { //time: O(n)
+        if boxesLeft == 0 { break }
+        numB, numU := boxTypes[i][0], boxTypes[i][1]
+        numB = min(numB, boxesLeft)
+        totalUnits += (numB*numU)
+        boxesLeft -= numB
+    }
+    return totalUnits
+    
+}
+
+func min(a, b int) int {
+    if a <= b {
+        return a
+    }
+    return b
+}
