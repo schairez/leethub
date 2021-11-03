84da5703708bf5d375223344b253b4f5c7a1760c
Sergio Chairez <schairezv@gmail.com>
2021-11-02 22:01:49 -0700

    Time: 8 ms (92.39%), Space: 4.6 MB (95.63%) - LeetHub

--- merge-intervals/merge-intervals.go
+++ merge-intervals/merge-intervals.go
@@ -1,35 +1,8 @@
-import (
-    "sort"
-    "fmt"
-)
+import "sort"
+//time: O(nlogn)
+//space: O(1)
 
 //an array of intervals where intervals[i] = [starti, endi]
-
-
-/*
-
-[[1,4],[0,2],[3,5]]
-sort -> [[0,2],[1,4],[3,5]]
-res -> [[0,2]]
-
-var (
-    prevE, prevS
-    currS, currE
-)
-res := [][]int{intervals[0]}
-check := 0
-for i:= 1; i < len(intervals); i++ {
-    prevE, prevS = res[check][0], res[check][1] 
-    currS, currE = intervals[i][0], intervals[i][1]    
-    if currS > prevE {
-        res = append(res, intervals[i])
-    } else {       // else if currS <= prevE 
-        res = append(res, min(prevS,))
-    } 
-}
-
-
-*/
 
 func merge(intervals [][]int) [][]int {
     //1 <= intervals.length <= 104
@@ -42,10 +15,7 @@ func merge(intervals [][]int) [][]int {
         }
         return intervals[i][0] < intervals[j][0]
     })
-    fmt.Println(intervals)
     res := [][]int{intervals[0]}
-    fmt.Println(res)
-    //prevS, prevE := res[0][0], res[0][1] 
     //check := 0
     for i:= 1; i < len(intervals); i++ {
         prevS, prevE := res[len(res)-1][0], res[len(res)-1][1]
@@ -60,10 +30,6 @@ func merge(intervals [][]int) [][]int {
     return res
 }
 
-func min(a,b int) int {
-    if a <= b  { return a }
-    return b
-}
 func max(a,b int) int {
     if a >= b { return a }
     return b
@@ -71,45 +37,3 @@ func max(a,b int) int {
 
 
 
-
-/*
-func merge(intervals [][]int) [][]int {
-    //1 <= intervals.length <= 104
-    if len(intervals) == 1 {
-        return intervals
-    }
-    sort.Slice(intervals, func(i, j int) bool {
-        return intervals[i][0] < intervals[j][0]
-    })
-    res := [][]int{}
-    fmt.Println(intervals)
-    var currStart, currEnd int
-    prevStart, prevEnd := intervals[0][0], intervals[0][1]
-    res = append(res, intervals[0])
-    for i:=1; i < len(intervals); i++ {
-        currStart, currEnd = intervals[i][0], intervals[i][1]
-        if prevStart -1 >= currStart || prevStart == 1 - currStart ||
-        prevEnd >= 1 +  currStart  || prevEnd == currEnd || prevStart == currStart {
-            res = res[1:]
-            res = append(res, []int{min(prevStart,currStart), max(currStart, currEnd)})
-        } else {
-            res = append(res, intervals[i])
-        } 
-        prevStart, prevEnd = currStart, currEnd
-    }
-    return res
-}
-*/
-
-
-
-/*
-        if prevStart >= currStart {
-            res = res[1:]
-            res = append(res, []int{currStart, currEnd})
-        } else if prevEnd >= currStart {
-            res = res[1:]
-            res = append(res, []int{prevStart, currEnd})
-        } else {
-
-*/
