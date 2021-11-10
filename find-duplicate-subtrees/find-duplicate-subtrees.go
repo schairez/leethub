6212ef30c2951301277b6c11def50ddda1c6e7c6
Sergio Chairez <schairezv@gmail.com>
2021-11-09 18:01:12 -0800

    Time: 28 ms (24.00%), Space: 43.8 MB (10.00%) - LeetHub

--- find-duplicate-subtrees/find-duplicate-subtrees.go
+++ find-duplicate-subtrees/find-duplicate-subtrees.go
@@ -4,6 +4,7 @@
 import (
     "fmt"
     "strconv"
+    "strings"
 )
 
 /**
@@ -14,6 +15,46 @@ import (
  *     Right *TreeNode
  * }
  */
+
+
+
+func findDuplicateSubtrees(root *TreeNode) []*TreeNode {
+    res := []*TreeNode{}
+    pathMap := map[string]int{}
+    //postorder
+    var dfs func(node *TreeNode ) string
+    dfs = func(node *TreeNode) string {
+        if node == nil { return "#" }
+        
+        left, right := dfs(node.Left), dfs(node.Right)
+        path := strings.Builder{}
+        path.WriteString("L")
+        path.WriteString(left)
+        path.WriteString(strconv.Itoa(node.Val))
+        path.WriteString("R")
+        path.WriteString(right)
+        
+        pathStr := path.String()
+        if _, ok := pathMap[pathStr]; !ok {
+            pathMap[pathStr] = 1
+        } else {
+            pathMap[pathStr]++
+        } 
+        if pathMap[pathStr] == 2 {
+            res = append(res, node)
+        }
+        return pathStr
+    }
+    dfs(root)
+    fmt.Println(pathMap)
+    return res
+}
+
+/*
+
+//time: O(n)
+//space: O(n)
+
 func findDuplicateSubtrees(root *TreeNode) []*TreeNode {
     res := []*TreeNode{}
     pathMap := map[string]int{}
@@ -39,3 +80,5 @@ func findDuplicateSubtrees(root *TreeNode) []*TreeNode {
     fmt.Println(pathMap)
     return res
 }
+
+*/
