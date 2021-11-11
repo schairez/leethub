feaee36b11506c6aacbc688ed055f3629621be1b
Sergio Chairez <schairezv@gmail.com>
2021-11-11 04:14:34 -0800

    Time: 0 ms (100.00%), Space: 2.3 MB (13.33%) - LeetHub

---
+++ nested-list-weight-sum/nested-list-weight-sum.go
@@ -0,0 +1,54 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * type NestedInteger struct {
+ * }
+ *
+ * // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ * func (n NestedInteger) IsInteger() bool {}
+ *
+ * // Return the single integer that this NestedInteger holds, if it holds a single integer
+ * // The result is undefined if this NestedInteger holds a nested list
+ * // So before calling this method, you should have a check
+ * func (n NestedInteger) GetInteger() int {}
+ *
+ * // Set this NestedInteger to hold a single integer.
+ * func (n *NestedInteger) SetInteger(value int) {}
+ *
+ * // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ * func (n *NestedInteger) Add(elem NestedInteger) {}
+ *
+ * // Return the nested list that this NestedInteger holds, if it holds a nested list
+ * // The list length is zero if this NestedInteger holds a single integer
+ * // You can access NestedInteger's List element directly if you want to modify it
+ * func (n NestedInteger) GetList() []*NestedInteger {}
+ */
+
+
+func depthSum(nestedList []*NestedInteger) int {
+    fmt.Println(nestedList)
+    res := 0
+    lvl := 1
+    var popElem *NestedInteger
+    var queue []*NestedInteger
+    queue = append(queue, nestedList...)
+    fmt.Println(len(queue))
+    fmt.Println()
+    for len(queue) > 0 {
+        n := len(queue)
+        for i :=0; i < n; i++ { 
+            popElem, queue = queue[0], queue[1:]
+            if popElem.IsInteger() {
+                intV := popElem.GetInteger()
+                res += lvl * intV
+            } else {
+                unnestedList := popElem.GetList()
+                queue = append(queue, unnestedList...)
+            }
+        }
+        lvl++
+    }
+    return res
+    
+    
+}
