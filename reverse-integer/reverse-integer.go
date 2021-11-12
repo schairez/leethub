4a4303d3dffe465e97c6208499eb2d1c54df88ac
Sergio Chairez <schairezv@gmail.com>
2021-11-11 20:23:59 -0800

    Time: 0 ms (100.00%), Space: 2.2 MB (100.00%) - LeetHub

--- reverse-integer/reverse-integer.go
+++ reverse-integer/reverse-integer.go
@@ -10,27 +10,38 @@ func checkValidI32(x int) bool {
     if x < minInt32 || x > maxInt32 {
         return false
     }
+    /*
+    if minIntNoLSB := minInt /10; x < minIntNoLSB {
+        return false
+    }
+    if maxIntNoLSB := maxInt/10; x > maxIntNoLSB
+    */
     return true 
 }
 
 func reverse(x int) int {
-    reverse_x := 0
+    reverseX := 0
     isNegative := false 
     if x < 0 {
         isNegative = true 
         x = -1 * x
     }
     for x > 0 {
-        reverse_x = reverse_x * 10 + x % 10 
-        x = x / 10
-        if !checkValidI32(reverse_x) {
+        lsb := x % 10
+        newRes := reverseX * 10 + lsb 
+        if ((newRes-lsb)/10 != reverseX) {
             return 0
         }
+        if !checkValidI32(newRes) {
+            return 0
+        }
+        x = x / 10
+        reverseX = newRes
     }
     if isNegative {
-        return -1 * reverse_x
+        return -1 * reverseX
     }
-    return reverse_x 
+    return reverseX 
     
     
     
