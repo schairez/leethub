29fc0eaa4aa6affd34261a9673751c94c53155c6
Sergio Chairez <schairezv@gmail.com>
2021-11-13 20:06:41 -0800

    Time: 4 ms (11.58%), Space: 2.3 MB (6.32%) - LeetHub

--- find-leaves-of-binary-tree/find-leaves-of-binary-tree.go
+++ find-leaves-of-binary-tree/find-leaves-of-binary-tree.go
@@ -6,29 +6,36 @@
  *     Right *TreeNode
  * }
  */
-import "fmt"
+//time: O(n)
+//space: O(n)
+
+
+//import "fmt"
 
 func findLeaves(root *TreeNode) [][]int {
     res := make([][]int,1)
     var postorder func(root *TreeNode, idx int) int
     postorder = func(root *TreeNode, idx int) int {
         if root == nil { return 0 }
-        //reached a leaf
+        
         idxL := postorder(root.Left, idx)
         idxR := postorder(root.Right, idx)
         idxNew := max(idxL, idxR)
+        
         if len(res) == idxNew {
             res = append(res, []int{})
         } 
-        fmt.Println(res)
+        //fmt.Println(res)
         res[idxNew] = append(res[idxNew], root.Val)
         root.Left = nil
         root.Right = nil
         return idxNew+1
     }
+    
     postorder(root, 0)
     return res
 }
+
 func max(a, b int) int {
     if a >= b { return a}
     return b
