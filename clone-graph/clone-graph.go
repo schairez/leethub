7c2eae5d4ba447c1743ae1058dffb7af757d9b0b
Sergio Chairez <schairezv@gmail.com>
2021-11-15 04:54:22 -0800

    Time: 0 ms (100.00%), Space: 2.9 MB (94.74%) - LeetHub

---
+++ clone-graph/clone-graph.go
@@ -0,0 +1,48 @@
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Neighbors []*Node
+ * }
+ */
+
+
+func cloneGraph(node *Node) *Node {
+    if node == nil { return nil }
+    
+    var visited [101]bool
+    var cloneNodeMap [101]*Node
+    //cloneNodeMap := map[int]*Node{}
+    
+    
+    var dfs func(node *Node)
+    dfs = func(node *Node) {
+        if visited[node.Val] { return }
+        visited[node.Val] = true
+        nodeClone := &Node{Val: node.Val, Neighbors: make([]*Node, 0, len(node.Neighbors))}
+        cloneNodeMap[node.Val] = nodeClone
+        for _, srcNeighbor := range node.Neighbors {
+            srcId := srcNeighbor.Val
+            if !visited[srcId] {
+                dfs(srcNeighbor)
+            }
+            neighborClone := cloneNodeMap[srcId] 
+            nodeClone.Neighbors = append(nodeClone.Neighbors, neighborClone)
+        }
+    }
+    
+    if len(node.Neighbors) == 0 { 
+        dfs(node)
+        return cloneNodeMap[node.Val]
+    }
+    
+    for _, srcNeighbor := range node.Neighbors {
+        id := srcNeighbor.Val
+        if !visited[id] {
+            dfs(srcNeighbor)
+        }
+    }
+    return cloneNodeMap[node.Val]
+    
+}
+    
