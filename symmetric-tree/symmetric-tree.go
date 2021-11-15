f8e91e5ef4ae9391a0593dee09b2fd2beb375db0
Sergio Chairez <schairezv@gmail.com>
2021-11-14 22:30:22 -0800

    Time: 4 ms (19.94%), Space: 3.1 MB (20.57%) - LeetHub

--- symmetric-tree/symmetric-tree.go
+++ symmetric-tree/symmetric-tree.go
@@ -1,6 +1,3 @@
-//n, m total nodes in tree a and b ( copy of tree a)
-//time: O(n + n) -> (O(2n)) -> O(n)
-//space: O(n)
 
 /**
  * Definition for a binary tree node.
@@ -11,7 +8,10 @@
  * }
  */
 
-func isSymmetric(root *TreeNode) bool {
+//n, m total nodes in tree a and b ( copy of tree a)
+//time: O(n + n) -> (O(2n)) -> O(n)
+//space: O(n)
+func isSymmetricR(root *TreeNode) bool {
     var mirror func(a, b *TreeNode) bool
     mirror = func(a, b *TreeNode) bool {
         switch {
@@ -28,3 +28,37 @@ func isSymmetric(root *TreeNode) bool {
     return mirror(root, root)
 }
     
+
+type NodePair struct {
+    a, b *TreeNode
+}
+func (np *NodePair) Destruct() (*TreeNode, *TreeNode) {
+    //note in prod add validation code here
+    return np.a, np.b
+}
+
+
+//iterative v with q
+//time: O(n); space: O(n)
+func isSymmetric(root *TreeNode) bool {
+    queue := []*NodePair{}
+    queue = append(queue, &NodePair{root,root})
+    for len(queue) > 0 {
+        a, b := queue[0].Destruct()
+        queue = queue[1:]
+        switch {
+            case a == nil && b == nil:
+                continue
+            case a == nil || b == nil:
+                return false
+            case a.Val != b.Val:
+                return false
+            default:
+                queue = append(queue, &NodePair{a.Left, b.Right})
+                queue = append(queue, &NodePair{a.Right, b.Left})
+        }
+    }
+    return true
+}
+
+
