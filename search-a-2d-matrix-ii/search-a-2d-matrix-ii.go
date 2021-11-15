14b68a07f7537a06afb8803277a82db09eeee1c9
Sergio Chairez <schairezv@gmail.com>
2021-11-14 23:20:01 -0800

    Time: 24 ms (76.99%), Space: 6.9 MB (63.72%) - LeetHub

--- search-a-2d-matrix-ii/search-a-2d-matrix-ii.go
+++ search-a-2d-matrix-ii/search-a-2d-matrix-ii.go
@@ -1,8 +1,28 @@
+//search space reduction v
+//time : O(m + n)
+//space: O(1)
+func searchMatrix(matrix [][]int, target int) bool {
+    m, n := len(matrix), len(matrix[0])
+    curR, curC := m-1, 0
+    for curR >= 0 && curC < n {
+        curV := matrix[curR][curC] 
+        switch {
+        case curV == target:
+            return true 
+        case curV > target:
+            curR--
+        case curV < target:
+            curC++
+        }
+    }
+    return false
+}
+
+
 //m = numRows; n = numCols
 //space: O(1)
 //time: O(mlog(n))
-
-func searchMatrix(matrix [][]int, target int) bool {
+func searchMatrixBinSearchV(matrix [][]int, target int) bool {
     binSearch := func(arr []int, t int) bool {
         lo, hi := 0, len(arr) -1
         for lo <= hi {
