a3f431610e602f5a4b75d83986f260daf2511e85
Sergio Chairez <schairezv@gmail.com>
2021-11-14 22:53:53 -0800

    Time: 20 ms (96.90%), Space: 6.9 MB (63.72%) - LeetHub

---
+++ search-a-2d-matrix-ii/search-a-2d-matrix-ii.go
@@ -0,0 +1,26 @@
+//space:
+//time: O()
+
+func searchMatrix(matrix [][]int, target int) bool {
+    binSearch := func(arr []int, t int) bool {
+        lo, hi := 0, len(arr) -1
+        for lo <= hi {
+            m := lo + (hi - lo) >> 1 
+            if arr[m] == t { 
+                return true 
+            } else if arr[m] > t {
+                hi = m -1
+            } else {
+                lo = m +1
+            }
+        }
+        return false
+    }
+    m := len(matrix)
+    for y:=0; y < m; y++ {
+        if found := binSearch(matrix[y], target); found {
+            return true
+        } 
+    }
+    return false
+}
