922c4fc984e713124a93ae472eb2128cd718f815
Sergio Chairez <schairezv@gmail.com>
2021-11-16 18:17:24 -0800

    Time: 8 ms (94.22%), Space: 7.1 MB (61.27%) - LeetHub

--- all-paths-from-source-to-target/all-paths-from-source-to-target.go
+++ all-paths-from-source-to-target/all-paths-from-source-to-target.go
@@ -1,12 +1,11 @@
-import (
-    "fmt"
-)
+//time: O(2^N * N)
+//space : O(2^N * N)
 
 func allPathsSourceTarget(graph [][]int) [][]int {
     n := len(graph)
     res := [][]int{}
-    var dfs func(path []int, src, nei, dst int)
-    dfs = func(path []int, src, nei, dst int) {
+    var dfs func(path []int, nei, dst int)
+    dfs = func(path []int, nei, dst int) {
         nodeSoFar := nei
         if nodeSoFar == dst {
             tmp := make([]int, len(path))
@@ -16,16 +15,15 @@ func allPathsSourceTarget(graph [][]int) [][]int {
         }
         neighbors := graph[nodeSoFar]
         for _, nextNei := range neighbors {
-            //if !visited[strPath] {
-                path = append(path, nextNei)
-                dfs(path, nodeSoFar, nextNei, dst)
-                path = path[:len(path)-1]
-            }
+            path = append(path, nextNei)
+            dfs(path, nextNei, dst)
+            path = path[:len(path)-1]
+        }
     }
     src, dst := 0, n-1
     neighbors := graph[0]
     for _, nei := range neighbors {
-        dfs([]int{src, nei}, src, nei, dst)
+        dfs([]int{src, nei}, nei, dst)
     }
     return res
 }
