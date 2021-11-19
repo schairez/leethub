7e0dea2e05d905066d77b555eb157defb209b1e1
Sergio Chairez <schairezv@gmail.com>
2021-11-19 02:39:46 -0800

    Time: 16 ms (89.94%), Space: 8 MB (61.36%) - LeetHub

--- binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.go
+++ binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.go
@@ -7,25 +7,17 @@
  * }
  */
 
-func max(vals ...int) int {
-    maxV := vals[0]
-    for i:=1; i<len(vals);i++ {
-        if vals[i] > maxV {
-            maxV = vals[i]
-        }
-    }
-    return maxV
-}
-    
 func maxPathSum(root *TreeNode) int {
     globalMaxPath := -1 << 31
     
     postOrder(root, func(localMaxPath int) {
         globalMaxPath = max(globalMaxPath, localMaxPath)
     })
+    
     return globalMaxPath
 }
 
+    
 //post -> LRD
 func postOrder(node *TreeNode, f func(localMaxPath int)) int {
     if node == nil {
@@ -35,45 +27,16 @@ func postOrder(node *TreeNode, f func(localMaxPath int)) int {
     rightGain := max(postOrder(node.Right, f), 0) 
     //contribution of nodeVal and its l,r children
     maxPathWChildren := node.Val + leftGain + rightGain
+    
     f(maxPathWChildren)
     
     return node.Val + max(leftGain, rightGain)
 }
 
-
-
-/*
-
-func inOrder(curNode *TreeNode, f func(curNode *TreeNode)) {
-    if curNode == nil {
-        return
+func max(a, b int) int {
+    if a >= b {
+        return a
     }
-    inOrder(curNode.Left, f)
-    f(curNode)
-    inOrder(curNode.Right, f)
+    return b
 }
-
-
-func maxPathSum(root *TreeNode) int {
-    
-    max := func(a, b int) int {
-        if a >= b {
-            return a
-        }
-        return b
-    }
-    globalMaxPath := -1 << 31
-    localMaxPath := globalMaxPath
-    
-    inOrder(root, func(curNode *TreeNode) {
-        nodeV := curNode.Val
-        localMaxPath = max(localMaxPath + nodeV, nodeV)
-        globalMaxPath = max(globalMaxPath, localMaxPath)
-    })
-    
-    return globalMaxPath
-}
-
-*/
-
 
