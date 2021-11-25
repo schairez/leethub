8242276ade743348fceb81197d16b86bb39d2d3b
Sergio Chairez <schairezv@gmail.com>
2021-11-25 05:08:34 -0800

    Time: 8 ms (83.93%), Space: 4.3 MB (100.00%) - LeetHub

--- longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.go
+++ longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.go
@@ -1,26 +1,9 @@
-func max(a, b int) int { if a > b { return a}; return b}
-
 //time : O(n)
 //space: O(1)
-//bottom up with no dp table
-func findLengthOfLCIS(nums []int) int {
-    n := len(nums)
-    res := 1
-    cnt :=1
-    for i:=1; i < n; i++ {
-        if nums[i] > nums[i-1] {
-            cnt++
-        } else {
-            cnt = 1
-        }
-        res = max(res, cnt)
-    }
-    return res
-} 
-//time : O(n)
-//space: O(n)
 
-func findLengthOfLCISTab(nums []int) int {
+func max(a, b int) int { if a > b { return a}; return b}
+
+func findLengthOfLCIS(nums []int) int {
     //1 <= nums.length <= 104
     n := len(nums)
     res := 1
