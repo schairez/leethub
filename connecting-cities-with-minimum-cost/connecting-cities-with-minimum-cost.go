2f31b220157dfd67abf90e19c65358f6cd59e736
Sergio Chairez <schairezv@gmail.com>
2021-11-28 03:05:35 -0800

    Time: 84 ms (100.00%), Space: 7.5 MB (56.00%) - LeetHub

---
+++ connecting-cities-with-minimum-cost/connecting-cities-with-minimum-cost.go
@@ -0,0 +1,90 @@
+import "sort"
+
+type ByWeight [][]int
+
+func (w ByWeight) Len() int {
+    return len(w)
+}
+func (w ByWeight) Swap(i, j int) {
+    w[i], w[j] = w[j], w[i]
+}
+func (w ByWeight) Less(i, j int) bool {
+    if w[i][2] < w[j][2] {
+        return true
+    }
+    return false
+}
+
+type UF struct {
+    Par []int
+    Sze []int
+    Cnt int
+}
+func NewUF(n int) *UF {
+    par := make([]int, n)
+    sze := make([]int, n)
+    for i := range par {
+        par[i] = i
+        sze[i] = 1
+    }
+    cnt := 0 
+    return &UF{par, sze, cnt}
+}
+func (uf *UF) CntUnions() int {
+    return uf.Cnt
+}
+
+func (uf *UF) Connected(v, w int) bool {
+    vRoot, wRoot := uf.Find(v), uf.Find(w)
+    return vRoot == wRoot
+}
+
+func (uf *UF) Find(v int) int {
+    for v != uf.Par[v] {
+        uf.Par[v] = uf.Par[uf.Par[v]]
+        v = uf.Par[v]
+    }
+    return v
+}
+
+func (uf *UF) Union(v, w int) {
+    rootV, rootW := uf.Find(v), uf.Find(w)
+    if rootV == rootW { return }
+    if uf.Sze[rootV] <= uf.Sze[rootW] {
+        uf.Sze[rootV] += uf.Sze[rootW]
+        uf.Par[rootW] = rootV
+    } else {
+        uf.Sze[rootW] += uf.Sze[rootV]
+        uf.Par[rootV] = rootW
+    }
+    uf.Cnt++ //cnt of unions
+}
+
+func minimumCost(n int, connections [][]int) int {
+    uf := NewUF(n+1) //1 <= xi, yi <= n
+    sort.Sort(ByWeight(connections))
+    //fmt.Println(connections)
+    res := 0
+    for _, conn := range connections {
+        cityA, cityB := conn[0], conn[1]
+        cost := conn[2]
+        if !uf.Connected(cityA,cityB) {
+            uf.Union(cityA,cityB)
+            res += cost
+            if uf.CntUnions() == n-1 {
+                return res
+            }
+        }
+    }
+    fmt.Println(uf.CntUnions())
+    return -1
+    /*
+    graph := make([][]int, n)
+    for i := range graph {
+        graph[i] = make([]int,)
+    }
+    */
+    return -1
+    
+}
+
