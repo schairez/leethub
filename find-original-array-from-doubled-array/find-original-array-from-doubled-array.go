5770017f40b16ea3d5ab25e117e5174b15d59f3a
Sergio Chairez <schairezv@gmail.com>
2021-12-14 05:53:15 -0800

    Time: 727 ms (5.45%), Space: 14.8 MB (10.91%) - LeetHub

--- find-original-array-from-doubled-array/find-original-array-from-doubled-array.go
+++ find-original-array-from-doubled-array/find-original-array-from-doubled-array.go
@@ -1,7 +1,6 @@
 import "sort"
 
-//k = freq of unique elems
-//time: O(nlogn + nlogk) ~ O(nlogn); 
+//time: O(nlogn + n) ~ O(nlogn)
 //space: O(n)
 /*
 testCases
@@ -9,45 +8,36 @@ testCases
 [0,0,0,0]
 
 */
+
+
 func findOriginalArray(changed []int) []int {
     n := len(changed)
-    if n % 2 != 0 {
-        return []int{}
+   	if n & 1 == 1 {
+		return []int{}
+	}
+	freqMap := map[int]int{}
+	for _, num := range changed {
+		freqMap[num]++
+	}
+    //for testcases with duplicates
+   //e.g. [0,0,0,0]
+    nums := make([]int, 0, len(freqMap))
+    for k := range freqMap {
+        nums = append(nums, k)
     }
-    sort.Ints(changed) //O(nlogn)
-    expSizeRes := n /2
-    res := make([]int, 0, expSizeRes)
-    valFreqMap := make(map[int]int, n)
-    for _, val := range changed {
-        valFreqMap[val]++
-    }
-    for len(valFreqMap) != 0 {
-        for _, val := range changed {
-            cnt := valFreqMap[val]
-            switch {
-            case val == 0:
-                if cnt % 2 != 0 { //odd num vals
-                    return []int{}
-                }
-                for i:= 0; i < cnt/2; i++ {
-                    res = append(res, val)
-                }
-                delete(valFreqMap, val)
-            default:
-                //ex: [3,3,3,6,6,6]
-                if valFreqMap[val*2] < cnt {
-                    return []int{}
-                }
-                delete(valFreqMap, val)
-                for i := 0; i < cnt; i++ {
-                    res = append(res, val)
-                }
-                valFreqMap[val*2] -= cnt
-                if valFreqMap[val*2] == 0 {
-                    delete(valFreqMap, val*2)
-                }
-            }
-        }
-    }
-    return res
+	sort.Ints(nums)
+	ans := make([]int, 0)
+	for _, num := range nums {
+		if freqMap[2*num] < freqMap[num] {
+			return []int{}
+		}
+		for i := 0; i < freqMap[num]; {
+			ans = append(ans, num)
+			i++
+			freqMap[2*num]--
+		}
+	}
+    
+	return ans 
+    
 }
