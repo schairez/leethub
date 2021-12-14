6aa8b1835bbcc90ae1d348dd3eb21949c358b661
Sergio Chairez <schairezv@gmail.com>
2021-12-14 08:27:08 -0800

    Time: 5 ms (32.26%), Space: 2.9 MB (62.90%) - LeetHub

--- remove-k-digits/remove-k-digits.go
+++ remove-k-digits/remove-k-digits.go
@@ -2,23 +2,21 @@
 //space: O(n)
 
 func removeKdigits(num string, k int) string {
+    if k == len(num) {
+        return "0"
+    }
     var deque []byte
     for i:=0; i < len(num); i++ {
-        for k > 0 && len(deque) > 0 && deque[len(deque)-1] > num[i] {
+        for k != 0 && len(deque) > 0 && deque[len(deque)-1] > num[i] {
             deque = deque[:len(deque)-1]
             k--
         }
         deque = append(deque, num[i])
     }  
-    for k != 0 {
-        deque = deque[:len(deque)-1]
-        k--
-    }
-    for len(deque) !=0 && deque[0] == '0' {
+    deque = deque[:len(deque)-k]
+    
+    for len(deque) > 1 && deque[0] == '0' {
         deque = deque[1:]
-    }
-    if len(deque) == 0 {
-        return "0"
     }
     
     return string(deque)
