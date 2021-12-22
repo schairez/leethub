3ea6e1c61cd3687e9c2d1e4b97cdc25ecde028a4
Sergio Chairez <schairezv@gmail.com>
2021-12-22 04:03:40 -0800

    Time: 116 ms (65.79%), Space: 9.3 MB (98.68%) - LeetHub

--- buildings-with-an-ocean-view/buildings-with-an-ocean-view.go
+++ buildings-with-an-ocean-view/buildings-with-an-ocean-view.go
@@ -1,6 +1,14 @@
+
+
 /*
 ocean view if all buildings to its right are smaller
 */
+
+
+
+//1 <= heights[i] <= 10^9
+
+//approach 1
 
 //time: O(n)
 //space: O(n)
@@ -15,3 +23,25 @@ func findBuildings(heights []int) []int {
     }
     return stack
 }
+
+
+
+//approach 2
+//time: O(n)
+//space: O(n)
+func findBuildings2(heights []int) []int {
+    maxHeight := 0  
+    n := len(heights)
+    res := make([]int, 0, n)
+    for i := n-1; i >= 0; i-- {
+        if heights[i] > maxHeight {
+            res = append(res, i)
+            maxHeight = heights[i]
+        }
+    }
+    for start, end := 0, len(res)-1; start < end; start, end = start+1, end-1 {
+        res[start], res[end] = res[end], res[start]
+    }
+    return res
+}
+
