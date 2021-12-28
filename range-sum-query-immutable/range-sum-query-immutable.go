c1279f8da2a78ea56f3ccb4ee02a65712a7f321b
Sergio Chairez <schairezv@gmail.com>
2021-12-27 22:01:42 -0800

    Time: 28 ms (89.66%), Space: 9.6 MB (62.93%) - LeetHub

--- range-sum-query-immutable/range-sum-query-immutable.go
+++ range-sum-query-immutable/range-sum-query-immutable.go
@@ -1,5 +1,47 @@
+//prefix sum approach
+//space: O(n)
+//time: O(1) for query, for construction: O(n)
+
+type NumArray struct {
+    prefixSumArr []int
+}
+
+
+func Constructor(nums []int) NumArray {
+    n := len(nums)
+    runningSum := 0
+    prefixSumArr := make([]int, n+1)
+    prefixSumArr[0] = 0
+    for idx, num := range nums {
+        runningSum += num
+        prefixSumArr[idx+1] = runningSum
+    }
+    
+    return NumArray{ prefixSumArr }
+}
+
+//[-2, 0, 3, -5, 2, -1]
+//[0, -2, -2, 1, -4, -2, -3]
+//f(0,0) = prefix[1] - prefix[0]
+//f(0,2) = prefix[3] - prefix[0]
+//constraint:
+//0 <= left <= right < nums.length
+
+func (this *NumArray) SumRange(left int, right int) int {
+    return this.prefixSumArr[right+1] - this.prefixSumArr[left]
+}
+
+
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * obj := Constructor(nums);
+ * param_1 := obj.SumRange(left,right);
+ */
+
+
 /*
-segment Tree approach
+segment Tree approach; overkill for an immutable question xD
 time: O(n) for constjuctor
 time: O(logn) for sumQuery 
 
@@ -8,14 +50,14 @@ space: O(logn) for stack space in range query search
 
 */
 
-type NumArray struct {
+type NumArrayV2 struct {
     root *SegmentNode
     nums []int
     
 }
 
-func Constructor(nums []int) NumArray {
-    query := &NumArray{nums: nums}
+func ConstructorV2(nums []int) NumArrayV2 {
+    query := &NumArrayV2{nums: nums}
     start, end := 0, len(nums)-1
     query.root = query.buildTree(start, end)
     return *query
@@ -27,7 +69,7 @@ type SegmentNode struct {
     left, right *SegmentNode
 }
 //segment tree; postorder node traversal
-func (this *NumArray) buildTree(startIdx, endIdx int) *SegmentNode {
+func (this *NumArrayV2) buildTree(startIdx, endIdx int) *SegmentNode {
     node := &SegmentNode{startIdx: startIdx, endIdx: endIdx}
     if startIdx == endIdx {
         node.sum = this.nums[startIdx]
@@ -43,7 +85,7 @@ func (this *NumArray) buildTree(startIdx, endIdx int) *SegmentNode {
 func min(a, b int) int { if a <= b { return a }; return b }
 func max(a, b int) int { if a >= b { return a }; return b }
 
-func (this *NumArray) rangeSum(node *SegmentNode, left int, right int) int {
+func (this *NumArrayV2) rangeSum(node *SegmentNode, left int, right int) int {
     if node == nil || left > right || left > node.endIdx || right < node.startIdx {
         return 0
     }
@@ -59,13 +101,8 @@ func (this *NumArray) rangeSum(node *SegmentNode, left int, right int) int {
 }
 
 
-func (this *NumArray) SumRange(left int, right int) int {
+func (this *NumArrayV2) SumRange(left int, right int) int {
     return this.rangeSum(this.root, left, right)
 }
 
 
-/**
- * Your NumArray object will be instantiated and called as such:
- * obj := Constructor(nums);
- * param_1 := obj.SumRange(left,right);
- */
