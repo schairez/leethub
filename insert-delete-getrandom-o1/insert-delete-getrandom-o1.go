b924a62dfa0d1a1d3916bb96dfa4d01db5734614
Sergio Chairez <schairezv@gmail.com>
2021-12-29 23:05:30 -0800

    Time: 369 ms (18.06%), Space: 55.9 MB (28.47%) - LeetHub

---
+++ insert-delete-getrandom-o1/insert-delete-getrandom-o1.go
@@ -0,0 +1,57 @@
+import "math/rand"
+
+//time: O(1) avg; O(n) when resizeable array resizes
+//space: O(n)
+
+type RandomizedSet struct {
+    //maps val to idx
+    numToIdx map[int]int
+    //maps idx to val
+    elems []int
+}
+
+
+func Constructor() RandomizedSet {
+    return RandomizedSet{ map[int]int{}, []int{} }
+}
+
+
+func (this *RandomizedSet) Insert(val int) bool {
+    if _, exists := this.numToIdx[val]; exists {
+        return false
+    }
+    n := len(this.elems)
+    this.elems = append(this.elems, val)
+    this.numToIdx[val] = n
+    return true
+}
+
+
+func (this *RandomizedSet) Remove(val int) bool {
+    if _, exists := this.numToIdx[val]; !exists {
+        return false
+    }
+    lastIdx := len(this.elems) - 1
+    lastElem := this.elems[lastIdx]
+    idxLoc := this.numToIdx[val]
+    this.elems[idxLoc] = lastElem
+    this.numToIdx[lastElem] = idxLoc
+    this.elems = this.elems[:lastIdx]
+    delete(this.numToIdx, val)
+    return true
+}
+
+
+func (this *RandomizedSet) GetRandom() int {
+    idx := rand.Intn(len(this.elems))
+    return this.elems[idx]
+}
+
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Insert(val);
+ * param_2 := obj.Remove(val);
+ * param_3 := obj.GetRandom();
+ */
