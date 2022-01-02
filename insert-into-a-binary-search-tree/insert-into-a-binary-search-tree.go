d5896d0442d13aa830fa497a3cac97771039b59c
Sergio Chairez <schairezv@gmail.com>
2022-01-02 05:45:55 -0800

    Time: 20 ms (99.35%), Space: 7.5 MB (22.58%) - LeetHub

--- insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.go
+++ insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.go
@@ -6,18 +6,45 @@
  *     Right *TreeNode
  * }
  */
-//if balanced tree height is logn = H
-//recursive call stack time and space avg case: O(log(n))
-//worst case: O(n)
+
+
+//time: O(n) worst case, best case O(logn) if balanced BST
+//space: O(1)
 func insertIntoBST(root *TreeNode, val int) *TreeNode {
     if root == nil {
         newNode := &TreeNode{Val: val}
         return newNode
     }
+    currNode := root
+    for currNode.Val != val {
+        if currNode.Val < val {
+            rightNextNode := currNode.Right
+            if rightNextNode == nil {
+                currNode.Right = &TreeNode{Val: val}
+            }
+            currNode = currNode.Right
+        } else {
+            leftNextNode := currNode.Left
+            if leftNextNode == nil {
+                currNode.Left = &TreeNode{Val: val}
+            }
+            currNode = currNode.Left
+        }
+    }
+    return root
+}
+//if balanced tree height is logn = H
+//recursive call stack time and space avg case: O(log(n))
+//worst case: O(n)
+func insertIntoBSTRecursive(root *TreeNode, val int) *TreeNode {
+    if root == nil {
+        newNode := &TreeNode{Val: val}
+        return newNode
+    }
     if root.Val < val {
-        root.Right = insertIntoBST(root.Right, val)
+        root.Right = insertIntoBSTRecursive(root.Right, val)
     } else {
-        root.Left = insertIntoBST(root.Left, val)
+        root.Left = insertIntoBSTRecursive(root.Left, val)
     }
     return root
 }
