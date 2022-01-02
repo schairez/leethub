f4ca8a5878d80cf52f1c4a9c0d89b43db68de329
Sergio Chairez <schairezv@gmail.com>
2022-01-02 06:44:20 -0800

    Time: 15 ms (24.29%), Space: 5.8 MB (37.57%) - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.go
+++ top-k-frequent-elements/top-k-frequent-elements.go
@@ -3,34 +3,38 @@ time: O(n)
 space: O(n)
 */
 
-import "fmt"
+
+func max(a, b int) int { if a >=b { return a}; return b}
 
 func topKFrequent(nums []int, k int) []int {
     if len(nums) == 1 {
         return nums
     }
-    res := make([]int, 0, k)
+    maxFreq := 0
+    //freqMap of num to cnt
     freqMap := make(map[int]int)
     for _, num := range nums {
         if _, ok := freqMap[num]; !ok {
             freqMap[num] = 0 
         }
         freqMap[num]++
+        maxFreq = max(maxFreq, freqMap[num])
     }
     //inverse frequency
+    //maps freq cnt's to arrlist of nums
     invFreq := map[int][]int{}
-    for k,v := range freqMap {
-        if _, ok := invFreq[v]; !ok {
-            invFreq[v] = []int{}
+    for numKey, cntVal := range freqMap {
+        if _, ok := invFreq[cntVal]; !ok {
+            invFreq[cntVal] = []int{}
         } 
-        invFreq[v] = append(invFreq[v], k)
+        invFreq[cntVal] = append(invFreq[cntVal], numKey)
     }
-    fmt.Println(invFreq)
-    for i:=len(nums); i >=0; i-- {
+    res := make([]int, 0, k)
+    for currFreq := maxFreq; currFreq >=0; currFreq-- {
         if len(res) == k {
             break
         }
-        if freqElemsArr, ok := invFreq[i]; ok {
+        if freqElemsArr, ok := invFreq[currFreq]; ok {
             res = append(res, freqElemsArr...)
         }
     }
