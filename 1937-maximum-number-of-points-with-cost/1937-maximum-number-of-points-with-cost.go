48015b5164d575c213ad7721d5c1a51e1cac1f27
Sergio Chairez <schairezv@gmail.com>
2022-01-08 08:38:51 -0800

    Time: 410 ms (52.17%), Space: 16.5 MB (39.13%) - LeetHub

--- 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.go
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.go
@@ -4,29 +4,27 @@ space: O(numC)
 
 */
 
-func max(a, b int) int { if a >= b { return a}; return b }
+func max(a, b int64) int64 { if a >= b { return a}; return b }
 
 func maxPoints(points [][]int) int64 {
-    var maxVal int64
     numRows := len(points)
     numCols := len(points[0])
     if numRows == 1 {
+        var maxVal int64
         for _, val := range points[0] {
-            if int64(val) > maxVal {
-                maxVal = int64(val)
-            } 
+            maxVal = max(maxVal, int64(val))
         }
         return maxVal
     }
     //max coords at level
-    dp := make([]int, numCols)
+    dp := make([]int64, numCols)
     for idx, pt := range points[0] {
-        dp[idx] = int(pt)
+        dp[idx] = int64(pt)
     }
-    fromLeft := make([]int, numCols)
-    fromRight := make([]int, numCols)
+    fromLeft := make([]int64, numCols)
+    fromRight := make([]int64, numCols)
     for row := 1; row < numRows; row++ {
-        prev := make([]int, numCols)
+        prev := make([]int64, numCols)
         copy(prev, dp)
         fromLeft[0] = prev[0]
         for col := 1; col < len(points[row]); col++ {
@@ -41,14 +39,13 @@ func maxPoints(points [][]int) int64 {
         }
         for col := 0; col < numCols; col++ {
             dp[col] = max(fromLeft[col], 
-                          fromRight[col]) + int(points[row][col])
+                          fromRight[col]) + int64(points[row][col])
         }
     }  
+    var maxVal int64
     //max number of pts with cost candidates
     for _, val := range dp {
-        if int64(val) > maxVal {
-            maxVal = int64(val)
-        } 
+        maxVal = max(maxVal, val)
     }
     return maxVal
 }
