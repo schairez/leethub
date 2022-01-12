89c9b6090e826c9e39e2f635940c5fe225ba7e2b
Sergio Chairez <schairezv@gmail.com>
2022-01-12 02:07:58 -0800

    Time: 76 ms (10.91%), Space: 9.8 MB (14.55%) - LeetHub

---
+++ 244-shortest-word-distance-ii/244-shortest-word-distance-ii.go
@@ -0,0 +1,78 @@
+type pair struct {
+    w1, w2 string
+}
+
+type WordDistance struct {
+    wordsLocMap map[string][]int
+    cacheMinDist map[pair]int
+}
+
+
+func Constructor(wordsDict []string) WordDistance {
+    wordsLocMap := make(map[string][]int)
+    for idx, word := range wordsDict {
+        if _, ok := wordsLocMap[word]; !ok {
+            wordsLocMap[word] = []int{}
+        }
+        wordsLocMap[word] = append(wordsLocMap[word], idx)
+    }
+    return WordDistance{wordsLocMap: wordsLocMap,
+                        cacheMinDist: make(map[pair]int),
+                       }
+    
+}
+
+
+/*
+At most 5000 calls will be made to shortest.
+- it's good to cache the distances and not repeat the same calculation again
+
+*/
+
+func (this *WordDistance) Shortest(word1 string, word2 string) int {
+    if _, ok := this.cacheMinDist[pair{word1, word2}]; ok {
+        return this.cacheMinDist[pair{word1, word2}]
+    }
+    if _, ok := this.cacheMinDist[pair{word2, word1}]; ok {
+        return this.cacheMinDist[pair{word2, word1}]
+    }
+    
+    shortestDist := (1 << 31)-1
+    //word1 and word2 are in wordsDict.
+    word1Locs, word2Locs := this.wordsLocMap[word1], this.wordsLocMap[word2]
+    w1Idx, w2Idx := 0, 0
+    for w1Idx != len(word1Locs) && w2Idx != len(word2Locs) {
+        distW1 := word1Locs[w1Idx]
+        distW2 := word2Locs[w2Idx]
+        
+        shortestDist = min(shortestDist, abs(distW1 - distW2))
+        if shortestDist == 1 { break }
+        
+        if distW1 < distW2 {
+            w1Idx++
+        } else {
+            w2Idx++
+        }
+    }
+    this.cacheMinDist[pair{word1, word2}] = shortestDist
+    
+    return shortestDist
+    
+}
+
+func abs(a int) int {
+    if a < 0 {
+        return -a
+    }
+    return a
+}
+
+func min(a, b int) int { if a <= b { return a}; return b }
+
+
+
+/**
+ * Your WordDistance object will be instantiated and called as such:
+ * obj := Constructor(wordsDict);
+ * param_1 := obj.Shortest(word1,word2);
+ */
