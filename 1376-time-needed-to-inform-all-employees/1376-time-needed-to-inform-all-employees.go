6e3e3bab89f4a49371855a96b4e964d4c70fa37e
Sergio Chairez <schairezv@gmail.com>
2022-01-13 02:31:26 -0800

    Time: 668 ms (9.30%), Space: 47.6 MB (6.98%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.go
@@ -0,0 +1,39 @@
+//time: O(N)
+//space: O(N)
+
+/*
+- each employee has one direct mgr
+1 <= n <= 105
+0 <= headID < n
+*/
+
+type pair struct {
+    nodeID, timeSoFar int
+}
+
+func max(a, b int) int { if a >= b { return a}; return b}
+
+func numOfMinutes(n int, headID int, manager []int, informTime []int) int {
+    graph := make([][]int, n)
+    //idx is emp; value is mgr of emp 
+    for emp, mgr := range manager {
+        if mgr != -1 {
+            graph[mgr] = append(graph[mgr], emp)
+        }
+    }
+    //time needed to inform all employees
+    timeNeeded := 0
+    //bfs approach
+    queue := make([]pair, n)
+    queue = append(queue, pair{headID, informTime[headID]})
+    var pollNode pair
+    for len(queue) != 0 {
+        pollNode, queue = queue[0], queue[1:]
+        timeNeeded = max(timeNeeded, pollNode.timeSoFar)
+        for _, subordinate := range graph[pollNode.nodeID] {
+            queue = append(queue, pair{subordinate,
+                                       pollNode.timeSoFar + informTime[subordinate]})
+        }
+    }
+    return timeNeeded
+}
