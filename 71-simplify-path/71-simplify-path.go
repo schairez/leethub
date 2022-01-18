0d0abcfe2731c8e41c3b7603301cb12b0342b434
Sergio Chairez <schairezv@gmail.com>
2022-01-18 14:47:26 -0800

    Time: 0 ms (100.00%), Space: 3.3 MB (62.71%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.go
@@ -0,0 +1,49 @@
+import "strings"
+
+//time: O(n)
+//space: O(n)
+
+func simplifyPath(path string) string {
+    var (
+        dirPath []string
+        deque []string
+        sb strings.Builder
+    )
+    dirPath = strings.Split(path, "/")
+    sb.Grow(len(dirPath))
+    for _, dir := range dirPath {
+        if dir == "." || dir == "" {
+            continue
+        } else if dir == ".." {
+            if len(deque) != 0 {
+                deque = deque[:len(deque)-1]
+            }
+        } else {
+            deque = append(deque, dir)
+        }
+    }
+    var poppedItem string
+    for len(deque) != 0 {
+        sb.WriteByte('/')
+        poppedItem, deque = deque[0], deque[1:]
+        sb.WriteString(poppedItem)
+    }
+    res := sb.String()
+    if len(res) == 0 {
+        return "/"
+    }
+    return res
+}
+
+
+/*
+ "/home//foo/"
+ 
+ -
+ foo
+ -
+ -
+ home
+ -
+ 
+*/
