15539dbbebb8080ad372e7bd3968f3d1237223da
Sergio Chairez <schairezv@gmail.com>
2022-01-31 14:23:34 -0800

    Time: 3 ms (40.57%), Space: 2.6 MB (98.87%) - LeetHub

---
+++ 206-reverse-linked-list/206-reverse-linked-list.go
@@ -0,0 +1,94 @@
+//recursive implementation
+// time: O(n) 
+// space: O(n) stack trace
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+/*
+1 -> 2 
+
+
+*/
+
+func reverseList(head *ListNode) *ListNode {
+    if head == nil || head.Next == nil {
+        return head
+    }
+    var prev, curr *ListNode
+    curr = head
+    for curr != nil {
+        curr.Next, prev, curr  = prev, curr, curr.Next  
+    }
+    return prev
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+func reverseList(head *ListNode) *ListNode {
+    if head == nil {
+        return nil 
+    }
+    //curr = head; prev = nil
+    return rev(head, nil)
+}
+ 
+func rev( currTail, prev *ListNode ) *ListNode {
+    nextIter := currTail.Next
+    currTail.Next = prev
+    //recurse until we find tail nil ptr 
+    if nextIter == nil {
+        return currTail
+    }
+    head := rev(nextIter, currTail)
+    return head
+}
+
+*/
+
+
+/*
+ _rev(1, nil)   nil
+     |          |
+--rev(2,1)      1
+     |          |   ^
+   rev(3,2)     2
+     |          |   ^
+   rev(4,3)     3
+     |          |
+   rev(5,4)     |   ^
+      |         4
+   rev(nil, 5)  |   ^
+                5
+                
+*/
