3c9bca4679ce43b968cb2593f3a45aad3cdebb6b
Sergio Chairez <schairezv@gmail.com>
2022-02-02 15:09:17 -0800

    Time: 0 ms (100.00%), Space: 2.3 MB (100.00%) - LeetHub

---
+++ 156-binary-tree-upside-down/156-binary-tree-upside-down.go
@@ -0,0 +1,151 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+
+//newRoot = prevLeft
+//newRoot.Right = prevRoot
+//newRoot.Left = prevRoot.Right
+
+func upsideDownBinaryTree(root *TreeNode) *TreeNode {
+    if root == nil || root.Left == nil && root.Right == nil {
+        return root
+    } 
+    var (
+        currNode, prevLeft, prevRight *TreeNode
+        prevRoot, nextLvlLeft, nextLvlRight *TreeNode
+    )
+    currNode = root
+    for {
+        nextLvlLeft, nextLvlRight = currNode.Left, currNode.Right
+        currNode.Left, currNode.Right = prevRight, prevRoot
+        if nextLvlLeft == nil {
+            break
+        }
+        prevRoot = currNode
+        prevLeft, prevRight = nextLvlLeft, nextLvlRight 
+        currNode = prevLeft
+        
+    }
+    return currNode
+} 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+/*    
+    
+func upsideDownBinaryTree(root *TreeNode) *TreeNode {
+    //newRoot -> prevLeft
+    //newRoot.Right -> prevRoot
+    //newRoot.Left = prevRight
+    var (
+        prevRoot, prevRight *TreeNode
+        currNode, nextLvlLeft, nextLvlRight *TreeNode
+    )
+    currNode = root
+    //continue traversing until next level children is nil
+    for {
+        //cache the child left and right nodes for next iter
+        //these will become the next root and nex
+        nextLvlLeft, nextLvlRight = currNode.Left, currNode.Right
+        currNode.Left, currNode.Right = prevRight, prevRoot
+        if nextLvlLeft == nil {
+            break
+        }
+        currNode, prevRoot, prevRight  = nextLvlLeft, currNode, nextLvlRight
+    }
+    return currNode
+}
+    
+
+func upsideDownBinaryTreeRecursive(root *TreeNode) *TreeNode {
+    if root == nil {
+        return nil
+    }
+    if root.Left == nil && root.Right == nil {
+        return root
+    }
+    finalRootNode := upsideDownBinaryTree(root.Left)
+    newRootNode := root.Left
+    newRootNode.Right = root
+    newRootNode.Left = root.Right
+    root.Left, root.Right = nil, nil
+    
+    return finalRootNode
+}
+*/
