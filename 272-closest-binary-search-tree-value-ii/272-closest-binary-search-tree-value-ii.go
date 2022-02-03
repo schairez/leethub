5b29703e5017c5367015fb32aca611b36308dc97
Sergio Chairez <schairezv@gmail.com>
2022-02-02 16:13:24 -0800

    Time: 13 ms (17.39%), Space: 5.5 MB (95.65%) - LeetHub

---
+++ 272-closest-binary-search-tree-value-ii/272-closest-binary-search-tree-value-ii.go
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func abs(val float64) float64 {if val < 0 {return -val}; return val }
+
+
+func closestKValues(root *TreeNode, target float64, k int) []int {
+    res := make([]int, 0, k)
+    var inorder func(node *TreeNode)
+    inorder = func(node *TreeNode)  {
+        if node == nil {
+            return
+        }
+        inorder(node.Left)
+        if len(res) < k {
+            res = append(res, node.Val)
+        } else {
+            diff := abs(float64(node.Val) - target)
+            oldDiff := abs(float64(res[0]) - target)
+            if diff < oldDiff {
+                res = res[1:]
+                res = append(res, node.Val)
+            }
+        }
+        inorder(node.Right)
+    }
+    inorder(root) 
+    return res
+}
