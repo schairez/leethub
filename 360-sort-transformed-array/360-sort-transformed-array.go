34a3c82aa619faabe1ead2eba40b3f7b22f1c4a0
Sergio Chairez <schairezv@gmail.com>
2022-02-04 17:34:56 -0800

    Time: 3 ms (37.50%), Space: 2.7 MB (93.75%) - LeetHub

---
+++ 360-sort-transformed-array/360-sort-transformed-array.go
@@ -0,0 +1,55 @@
+//f(x) = ax^2 + bx + c
+func quadWithCoeff(a, b, c int) func(x int) int {
+    return func(x int) int {
+        return a * (x * x) + (b * x) + c
+    }
+}
+
+func abs(x int) int {
+    if x < 0 {
+        return -x
+    }
+    return x
+}
+
+//time: O(n)
+//space: O(1) if not considering res; O(n) if so
+
+//precondition: nums is sorted in non-decr order
+func sortTransformedArray(nums []int, a int, b int, c int) []int {
+    n := len(nums)
+    res := make([]int, n)
+    //quad with coefficients initialized
+    quadFn := quadWithCoeff(a, b, c)
+    idx := n-1
+    //upside down parabola
+    invParabolaCond := a < 0
+    if invParabolaCond {
+        idx = 0
+    }
+    var quadLeftV, quadRightV int
+    left, right := 0, n-1
+    for left <= right {
+        quadLeftV, quadRightV = quadFn(nums[left]), quadFn(nums[right])
+        if invParabolaCond {
+            if quadLeftV < quadRightV {
+                res[idx] = quadLeftV
+                left++
+            } else {
+                res[idx] = quadRightV
+                right--
+            }
+            idx++
+        } else {
+            if quadLeftV > quadRightV {
+                res[idx] = quadLeftV
+                left++
+            } else {
+                res[idx] = quadRightV
+                right--
+            }
+            idx--
+        }
+    }
+    return res
+}
