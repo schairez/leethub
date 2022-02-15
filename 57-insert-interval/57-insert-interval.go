b4e826b0391410008c34195cc73b5675859a7b1e
Sergio Chairez <schairezv@gmail.com>
2022-02-14 22:40:20 -0800

    Time: 11 ms (41.82%), Space: 4.7 MB (67.68%) - LeetHub

--- 57-insert-interval/57-insert-interval.go
+++ 57-insert-interval/57-insert-interval.go
@@ -1,10 +1,50 @@
 
+func min(a, b int) int { if a <= b { return a}; return b}
+func max(a, b int) int { if a >= b { return a}; return b}
+
+//57. Insert Interval
+// time: O(n)
+// space: O(n)
+// we add all the non-overlapping intervals first
+// in other words, all the intervals strictly left of newInterval 
+// next, we mutate newInterval if there is interval idx that
+// overlaps with newInterval
+// lastly, we append any remaining intervals from our intervals input
+func insert(intervals [][]int, newInterval []int) [][]int {
+    n := len(intervals)
+    if n == 0 {
+        return [][]int{newInterval}
+    }
+    var res [][]int
+    idx := 0
+    // loop breaks when endTime of input interval idx
+    // is within the newInterval range
+    for idx < n && intervals[idx][1] < newInterval[0] {
+        res = append(res, intervals[idx])
+        idx++
+    }
+    for idx < n && intervals[idx][0] <= newInterval[1] {
+        newInterval[0] = min(newInterval[0], intervals[idx][0])
+        newInterval[1] = max(newInterval[1], intervals[idx][1])
+        idx++
+    } 
+    res = append(res, newInterval)
+    for idx < n {
+        res = append(res, intervals[idx])
+        idx++
+    }
+    return res
+}
+
+// 1st version below; even though below passes and is time efficient
+// I don't recommend below b/c of all the edge cases I had to control for
+/*
+//57. Insert Interval
+//time: O(n)
+//space: O(n) output res space
 // ex: intervals = [[1,2], [3,5], [6,7]]; newInterval = [4,8]
-// 
-//
-//
+// ex: [[0,2],[3,3],[6,11]] newInterval=[9,15] 
 // ex: [[1,5]]; newInterval = [6,8]
-//
 
 func min(a, b int) int { if a <= b { return a}; return b}
 func max(a, b int) int { if a >= b { return a}; return b}
@@ -78,3 +118,4 @@ func insert(intervals [][]int, newInterval []int) [][]int {
     }
     return stack
 }
+*/
