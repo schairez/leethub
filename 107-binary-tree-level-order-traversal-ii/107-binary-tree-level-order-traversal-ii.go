33132d66e079fcb9c042241dda1a8ada9328d899
Sergio Chairez <schairezv@gmail.com>
2022-02-17 01:52:05 -0800

    Time: 6 ms (25.19%), Space: 2.9 MB (84.44%) - LeetHub

---
+++ 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.go
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func rev2DSlice(valsAtLvl [][]int) {
+    n := len(valsAtLvl)
+    start, end := 0, n-1
+    for start < end {
+        valsAtLvl[start], valsAtLvl[end] = valsAtLvl[end], valsAtLvl[start]
+        start++
+        end--
+    }
+}
+
+func levelOrderBottom(root *TreeNode) [][]int {
+    if root == nil {
+        return [][]int{}
+    }
+    var res [][]int
+    queue := []*TreeNode{root} 
+    var node *TreeNode
+    for len(queue) != 0 {
+        n := len(queue)
+        nodeVals := make([]int, 0, n)
+        for i := 0; i < n; i++ {
+            node, queue = queue[0], queue[1:]
+            nodeVals = append(nodeVals, node.Val)
+            if node.Left != nil {
+                queue = append(queue, node.Left)
+            }
+            if node.Right != nil {
+                queue = append(queue, node.Right)
+            }
+        }
+        res = append(res, nodeVals)
+    }
+    
+    rev2DSlice(res)
+    
+    return res
+}
