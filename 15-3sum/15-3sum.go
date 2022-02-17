83bd008e3d8ba751b5d1e67bbf3d54ec99c644ff
Sergio Chairez <schairezv@gmail.com>
2022-02-17 01:03:06 -0800

    Time: 29 ms (86.10%), Space: 7.4 MB (86.28%) - LeetHub

---
+++ 15-3sum/15-3sum.go
@@ -0,0 +1,44 @@
+
+//ex
+//nums = [-1,0,1,2,-1,-4]
+//sortedV -> [-4,-1,-1,0,1,2]
+
+//num1 + num2 + num3 = 0
+// num1 + num2 = -num3
+//  target = -num3
+
+//time: O(nlogn) + O(n*n) ≈ O(n*n)
+//space: O(logn) for go's internal sort
+func threeSum(nums []int) [][]int {
+    n := len(nums)
+    if n < 3 {
+        return [][]int{}
+    }
+    res := make([][]int, 0)
+    sort.Ints(nums) //O(nlogn) time
+    for i := 0; i < n && nums[i] <= 0; i++ {
+        target := - nums[i]
+        start, end := i + 1, n-1
+        if i == 0 || nums[i] != nums[i-1] {
+            for start < end {
+                twoSum := nums[start] + nums[end]
+                if twoSum == target {
+                    res = append(res, []int{nums[i], nums[start], nums[end]})
+                    for start != end && nums[start] == nums[start+1] {
+                        start++
+                    } 
+                    for start != end && nums[end] == nums[end-1] {
+                        end--
+                    }
+                    start++
+                    end--
+                } else if twoSum < target {
+                    start++
+                } else {
+                    end--
+                }
+            }
+        }
+    } 
+    return res
+}
