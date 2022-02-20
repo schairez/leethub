0c5746b96e86900f334cf7a786a298b0d6e9d0d9
Sergio Chairez <schairezv@gmail.com>
2022-02-19 16:55:52 -0800

    Time: 28 ms (84.50%), Space: 4.4 MB (66.50%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.go
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.go
@@ -1,7 +1,59 @@
 
+// 
+// we first iterate through n points, which the index represents the vertex node adding each node to our nodes slice
+// this takes O(V) time to iterate through input and O(logV) to heapify this point into our heap
+// in our while condition, we pop from our heap and iterate through all candidate edges that are unvisited, checking whether
+// this is a better cost than our initialized +inf to this node
+// this is done in |E| time, to pop from our heap in logV time 
+// time: O( |V|log|V| + |E|log|V|) ≈ O(|E|log|V|)
+//  
+// space: O(|V|) for heap memory and an additional O(|V|) for the nodes slice
+
 const (
     inf = 1 << 31 - 1
 )
+
+func minCostConnectPoints(points [][]int) int {
+    n := len(points)
+    nodes := make([]*PrimNode, n)
+    pq := make(PriorityQueue, 0 )
+    heap.Init(&pq)
+    for idx := range points {
+        node := &PrimNode{
+            vertexId: idx,
+            cost: inf,
+            visited: false,
+        }
+        heap.Push(&pq, node)
+        nodes[idx] = node
+    }
+    nodes[0].cost = 0
+    heap.Fix(&pq, nodes[0].index) //O(logn) operation
+    
+    numVisited := 0
+    minCost := 0
+    for numVisited < n && pq.Len() != 0 {
+        v1 := heap.Pop(&pq).(*PrimNode)
+        minCost += v1.cost
+        numVisited++
+        v1.visited = true
+        srcX, srcY := points[v1.vertexId][0], points[v1.vertexId][1]
+        for nei := 0; nei < n; nei++ {
+            v2 := nodes[nei]
+            if v2.visited {
+                continue
+            }
+            dstX, dstY := points[v2.vertexId][0], points[v2.vertexId][1]
+            //cost of connecting two points
+            if cost := manhattanDist(srcX, srcY, dstX, dstY); cost < nodes[v2.vertexId].cost {
+                v2.cost = cost
+                v2.predId = v1.vertexId
+                heap.Fix(&pq, v2.index)
+            }
+        }
+    }
+    return minCost
+}
 
 
 
@@ -48,45 +100,3 @@ func (pq *PriorityQueue) Pop() interface{} {
 }
 
 
-
-func minCostConnectPoints(points [][]int) int {
-    n := len(points)
-    nodes := make([]*PrimNode, n)
-    pq := make(PriorityQueue, 0 )
-    heap.Init(&pq)
-    for idx := range points {
-        node := &PrimNode{
-            vertexId: idx,
-            cost: inf,
-            visited: false,
-        }
-        heap.Push(&pq, node)
-        nodes[idx] = node
-    }
-    nodes[0].cost = 0
-    heap.Fix(&pq, nodes[0].index)
-    
-    numVisited := 0
-    minCost := 0
-    for numVisited < n && pq.Len() != 0 {
-        v1 := heap.Pop(&pq).(*PrimNode)
-        minCost += v1.cost
-        numVisited++
-        v1.visited = true
-        srcX, srcY := points[v1.vertexId][0], points[v1.vertexId][1]
-        for nei := 0; nei < n; nei++ {
-            v2 := nodes[nei]
-            if v2.visited {
-                continue
-            }
-            dstX, dstY := points[v2.vertexId][0], points[v2.vertexId][1]
-            //cost of connecting two points
-            if cost := manhattanDist(srcX, srcY, dstX, dstY); cost < nodes[v2.vertexId].cost {
-                v2.cost = cost
-                v2.predId = v1.vertexId
-                heap.Fix(&pq, v2.index)
-            }
-        }
-    }
-    return minCost
-}
