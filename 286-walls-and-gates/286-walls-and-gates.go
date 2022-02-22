8c351e0cac5d2aa646357509ed83a85c86c91c90
Sergio Chairez <schairezv@gmail.com>
2022-02-22 01:23:34 -0800

    Time: 84 ms (17.83%), Space: 9.2 MB (39.53%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.go
+++ 286-walls-and-gates/286-walls-and-gates.go
@@ -1,5 +1,8 @@
-//multi-src BFS approach
 
+//multi-src BFS approach
+// time: O(m*n) to find src gate nodes
+// space: O(m*n) since we can have a case where we insert a high number
+// of gates at once, and depends on growth of our rooms input
 
 func inArea(numR, numC int) func(int, int) bool {
     return func(i, j int) bool {
@@ -10,8 +13,6 @@ func inArea(numR, numC int) func(int, int) bool {
     }
 }
 
-    //seen := struct{}{}
-    //visited := make(map[int][int]seen)
 
 func wallsAndGates(rooms [][]int)  {
     numR, numC := len(rooms), len(rooms[0])
@@ -24,27 +25,19 @@ func wallsAndGates(rooms [][]int)  {
         inf = 1 << 31 - 1
     )
     var queue [][2]int
-    //todo: collect num emptyRooms?
-    numGates := 0
     for r := 0; r < numR; r++ {
         for c := 0; c < numC; c++ {
         if rooms[r][c] == gate {
             queue = append(queue, [2]int{r, c})
-            numGates++
             }
         }
     }
-    // TODO: if it's impossible to reach a gate fill with inf
-    //if numGates == 0 {
-        //return rooms or fill with inf?
-    //} 
     var r, c int
     var dist int
     for len(queue) != 0 {
         r, c = queue[0][0], queue[0][1]
         dist = rooms[r][c]
         queue = queue[1:]
-        //visited[r][c] = seen
         // N
         if inAreaFn(r-1, c) && rooms[r-1][c] == inf {
             rooms[r-1][c] = dist+1
