2a87d006315582745379cc3128dc0acbda10c718
Sergio Chairez <schairezv@gmail.com>
2022-02-23 02:49:06 -0800

    Time: 1946 ms (52.94%), Space: 36.9 MB (38.23%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.go
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.go
@@ -2,13 +2,15 @@
 
 func minKnightMoves(x int, y int) int {
     //visited := make(map[int]map[int]bool, 300)
-    visited := make(map[int]map[int]bool)
+    //visited := make(map[int]map[int]bool)
+    visited := make(map[[2]int]struct{})
     dX := [8]int{2, 2, -2, -2, 1, -1, 1, -1}
     dY := [8]int{1, -1, 1, -1, 2, 2, -2, -2}
     numMoves := 0
     // 8 possible next moves
-    visited[0] = make(map[int]bool)
-    visited[0][0] = true
+    //visited[0] = make(map[int]bool)
+    visited[[2]int{0,0}] = struct{}{}
+    //visited[0][0] = true
     var queue [][2]int //0 -> x, 1 -> y
     queue = append(queue, [2]int{0,0})
     var node [2]int 
@@ -22,14 +24,15 @@ func minKnightMoves(x int, y int) int {
             }
             for idx := 0; idx < 8; idx++ {
                 nextX, nextY := node[0] + dX[idx], node[1] + dY[idx]
-                if visX, existsX := visited[nextX]; existsX {
-                    if _, existsY := visX[nextY]; existsY {
-                        continue
-                    }
-                } else {
-                    visited[nextX] = make(map[int]bool)
+                key := [2]int{nextX, nextY}
+                //constraint: -300 <= x, y <= 300
+                if nextX < -300 || nextY > 300 {
+                    continue
                 }
-                visited[nextX][nextY] = true 
+                if _, exists := visited[key]; exists {
+                    continue
+                }
+                visited[key] = struct{}{}
                 queue = append(queue, [2]int{nextX, nextY})
             }
         }
