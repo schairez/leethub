d4641bad1be09b004102803fe642474c96366659
Sergio Chairez <schairezv@gmail.com>
2022-02-23 18:58:19 -0800

    Time: 116 ms (46.43%), Space: 8.2 MB (39.29%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.go
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.go
@@ -10,28 +10,28 @@
 func nextLargerNodes(head *ListNode) []int {
     type pair struct {val, idx int}
     var (
-        monoDecrStack = make([]pair, 0)
-        // constraint: 1 <= n <= 10^4
-        res           = make([]int, 10000)
+        monoStack          = make([]pair, 0)       // mono decr stack
+        res                = make([]int, 10000)    // constraint: 1 <= n <= 10^4
         idxSoFar           = -1
     )
     var top pair
     for head != nil {
         idxSoFar++
         currVal := head.Val
-        for len(monoDecrStack)!= 0 && currVal > monoDecrStack[len(monoDecrStack)-1].val {
-            lenStack := len(monoDecrStack)
-            top, monoDecrStack = monoDecrStack[lenStack-1], monoDecrStack[:lenStack-1]
+        for stackSize := len(monoStack);
+        stackSize != 0 && currVal > monoStack[stackSize-1].val;
+        stackSize = len(monoStack) {
+            top, monoStack = monoStack[stackSize-1], monoStack[:stackSize-1]
             res[top.idx] = currVal
         } 
-        monoDecrStack = append(monoDecrStack, pair{currVal, idxSoFar})
+        monoStack = append(monoStack, pair{currVal, idxSoFar})
         head = head.Next
     }
-    for len(monoDecrStack)!= 0 {
-        lenStack := len(monoDecrStack)
-        top, monoDecrStack = monoDecrStack[lenStack-1], monoDecrStack[:lenStack-1]
+    res = res[:idxSoFar+1]
+    for stackSize := len(monoStack); stackSize != 0; stackSize = len(monoStack) {
+        top, monoStack = monoStack[stackSize-1], monoStack[:stackSize-1]
         res[top.idx] = 0
     }
-    return res[:idxSoFar+1]
+    return res
 }
 
