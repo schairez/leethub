4b3fdc78c8fa6dc480af3b858788c76210b131f0
Sergio Chairez <schairezv@gmail.com>
2022-02-23 18:48:43 -0800

    Time: 60 ms (89.29%), Space: 8.1 MB (39.29%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.go
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.go
@@ -13,18 +13,18 @@ func nextLargerNodes(head *ListNode) []int {
         monoDecrStack = make([]pair, 0)
         // constraint: 1 <= n <= 10^4
         res           = make([]int, 10000)
-        idx           = -1
+        idxSoFar           = -1
     )
     var top pair
     for head != nil {
-        idx++
+        idxSoFar++
         currVal := head.Val
         for len(monoDecrStack)!= 0 && currVal > monoDecrStack[len(monoDecrStack)-1].val {
             lenStack := len(monoDecrStack)
             top, monoDecrStack = monoDecrStack[lenStack-1], monoDecrStack[:lenStack-1]
             res[top.idx] = currVal
         } 
-        monoDecrStack = append(monoDecrStack, pair{currVal, idx})
+        monoDecrStack = append(monoDecrStack, pair{currVal, idxSoFar})
         head = head.Next
     }
     for len(monoDecrStack)!= 0 {
@@ -32,6 +32,6 @@ func nextLargerNodes(head *ListNode) []int {
         top, monoDecrStack = monoDecrStack[lenStack-1], monoDecrStack[:lenStack-1]
         res[top.idx] = 0
     }
-    return res[:idx+1]
+    return res[:idxSoFar+1]
 }
 
