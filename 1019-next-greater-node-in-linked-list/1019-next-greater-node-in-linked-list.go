e4408d9f8ff0b0319982947760b8817cb7973212
Sergio Chairez <schairezv@gmail.com>
2022-02-23 19:11:44 -0800

    Time: 55 ms (92.86%), Space: 8.3 MB (32.14%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.go
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.go
@@ -6,38 +6,11 @@
  * }
  */
 
-// time: O(n)
-// space: O(1) if not including res output 
-
-func nextLargerNodes(head *ListNode)  []int {
-    nextGreaterElem := func(node *ListNode, prevVal int) int {
-        if node == nil {
-            return 0
-        }
-        for node != nil && prevVal >= node.Val {
-            node = node.Next
-        }
-        if node == nil {
-            return 0
-        }
-        return node.Val
-    }
-    
-    res := make([]int, 10000)    // constraint: 1 <= n <= 10^4
-    idxSoFar := -1
-    for head != nil {
-        idxSoFar++
-        res[idxSoFar] = nextGreaterElem(head.Next, head.Val)
-        head = head.Next
-    }
-    return res[:idxSoFar+1]
-}
-
 
 // mono decr stack version
 // time: O(n)
 // space: O(n)
-func nextLargerNodesV2(head *ListNode) []int {
+func nextLargerNodes(head *ListNode) []int {
     type pair struct {val, idx int}
     var (
         monoStack          = make([]pair, 0)       // mono decr stack
@@ -60,6 +33,35 @@ func nextLargerNodesV2(head *ListNode) []int {
     for stackSize := len(monoStack); stackSize != 0; stackSize = len(monoStack) {
         top, monoStack = monoStack[stackSize-1], monoStack[:stackSize-1]
         res[top.idx] = 0
+    }
+    return res[:idxSoFar+1]
+}
+
+
+
+// time: O(n^2)
+// space: O(1) if not including res output 
+
+func nextLargerNodesV2(head *ListNode)  []int {
+    nextGreaterElem := func(node *ListNode, prevVal int) int {
+        if node == nil {
+            return 0
+        }
+        for node != nil && prevVal >= node.Val {
+            node = node.Next
+        }
+        if node == nil {
+            return 0
+        }
+        return node.Val
+    }
+    
+    res := make([]int, 10000)    // constraint: 1 <= n <= 10^4
+    idxSoFar := -1
+    for head != nil {
+        idxSoFar++
+        res[idxSoFar] = nextGreaterElem(head.Next, head.Val)
+        head = head.Next
     }
     return res[:idxSoFar+1]
 }
