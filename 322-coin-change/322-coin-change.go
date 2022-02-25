d0a75a6d1dad788df889fce63db7da9238cb815c
Sergio Chairez <schairezv@gmail.com>
2022-02-24 22:11:29 -0800

    Time: 14 ms (72.90%), Space: 7.7 MB (24.04%) - LeetHub

--- 322-coin-change/322-coin-change.go
+++ 322-coin-change/322-coin-change.go
@@ -2,6 +2,8 @@
 func min(a, b int) int { if a <= b { return a}; return b}
 
 //BFS approach
+// time: O(len(coins) * |amount|)
+// space: O(n) for queue and visited set
 
 func coinChange(coins []int, amount int) int {
     if amount == 0 {
@@ -33,7 +35,6 @@ func coinChange(coins []int, amount int) int {
     }
     return -1
 }
-    
  
 
 // dp approach
@@ -61,77 +62,13 @@ func coinChangeDP(coins []int, amount int) int {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-//Return the fewest number of
-coins that you need to make up that
-amount. If that amount of money cannot be made
-up by any combination of the coins, return -1.
-
-//goal: minimize number of coins s.t. dollarValue == amount
-
-Input: coins = [1,2,5], amount = 11
-Output: 3
-Explanation: 11 = 5 + 5 + 1
-
-constraints:
-1 <= coins.length <= 12
-1 <= coins[i] <= 2^31 -1
-0 <= amount <= 10^4
-
-*/
-
 //state transition
 //dp[i] = min(dp[i], dp[i - coins[i]] + 1)
 
 //time: O(amount*len(coins))
 //space: O(amount)
 /*
+//version from 12/5/2021
 
 func min(a, b int) int { if a <= b { return a }; return b }
 func coinChange(coins []int, amount int) int {
