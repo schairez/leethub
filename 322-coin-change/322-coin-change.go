ae3174c6158785ba8b83a959646e7759491bac1e
Sergio Chairez <schairezv@gmail.com>
2022-02-24 22:02:19 -0800

    Time: 20 ms (55.52%), Space: 7.2 MB (34.32%) - LeetHub

--- 322-coin-change/322-coin-change.go
+++ 322-coin-change/322-coin-change.go
@@ -1,7 +1,46 @@
 
 func min(a, b int) int { if a <= b { return a}; return b}
 
+//BFS approach
+
 func coinChange(coins []int, amount int) int {
+    if amount == 0 {
+        return 0
+    }
+    visited := make([]bool, amount+1)
+    visited[amount] = true
+    var (
+        queue []int
+        nodeAmtBalance  int
+        numCoins int
+    )
+    queue = append(queue, amount)
+    for len(queue) != 0 {
+        for currLen := len(queue); currLen != 0; currLen-- {
+            nodeAmtBalance, queue = queue[0], queue[1:]
+            if nodeAmtBalance == 0 {
+                return numCoins
+            }
+            for _, denomVal := range coins {
+                if remBalance := nodeAmtBalance - denomVal; remBalance >= 0 &&
+                !visited[remBalance] {
+                    visited[remBalance] = true
+                    queue = append(queue, remBalance)
+                }
+            }
+        }
+        numCoins++
+    }
+    return -1
+}
+    
+ 
+
+// dp approach
+// space: O(n) space for dp slice
+// time: O(|amount| * |coins|) 
+
+func coinChangeDP(coins []int, amount int) int {
     const maxInt32 = 1 << 31 -1
     dp := make([]int, amount+1)
     for amt := 1; amt < len(dp); amt++ {
