4c3fd9d999c6952a0cfd171fff28cf44d5b59cc3
Sergio Chairez <schairezv@gmail.com>
2022-03-01 05:10:33 -0800

    Time: 0 ms (100.00%), Space: 6.6 MB (39.29%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.go
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.go
@@ -8,7 +8,45 @@
  * }
  */
 
+// time: O(n)
+// space: O(1)
 func connect(root *Node) *Node {
+    if root == nil {
+        return root
+    }
+    lvlStartNode := root
+    for lvlStartNode != nil {
+        var prevNode *Node
+        currNode := lvlStartNode
+        lvlStartNode = nil
+        for currNode != nil {
+            if currNode.Left != nil {
+                if prevNode != nil {
+                    prevNode.Next = currNode.Left
+                } else {
+                    lvlStartNode = currNode.Left
+                }
+                prevNode = currNode.Left
+            }
+            if currNode.Right != nil {
+                if prevNode != nil {
+                    prevNode.Next = currNode.Right
+                } else {
+                    lvlStartNode = currNode.Right
+                }
+                prevNode = currNode.Right
+            }
+            currNode = currNode.Next
+        }
+    }
+    return root
+}
+
+// BFS approach
+// time: O(n)
+// space: O(maxWidthLvl) ≈ O(n)
+
+func connectBFS(root *Node) *Node {
     if root == nil {
         return root
     }
