7ef3dd33967d5dadc6c9271a757e3bc6c21381eb
Sergio Chairez <schairezv@gmail.com>
2022-02-28 18:48:58 -0800

    Time: 53 ms (89.25%), Space: 8.7 MB (52.34%) - LeetHub

--- 721-accounts-merge/721-accounts-merge.go
+++ 721-accounts-merge/721-accounts-merge.go
@@ -1,8 +1,15 @@
 
+//
+//
+//
+//
 
 func accountsMerge(accounts [][]string) [][]string {
+    // space: O(n)
     emailToID := make(map[string]int)
     emailToName := make(map[string]string)
+    
+    //time: O(len(accounts) * len(acct))
     for _, acct := range accounts {
         name := acct[0]
         for _, email := range acct[1:] {
@@ -12,11 +19,14 @@ func accountsMerge(accounts [][]string) [][]string {
             }
         }
     }
-    //ufSize := make([]int, len(emailToID)) 
+    ufSize := make([]int, len(emailToID))
     ufParent := make([]int, len(emailToID)) 
     for idx := range ufParent {
         ufParent[idx] = idx
+        ufSize[idx] = 1
     }
+    
+    // inv Ackerman O(a(n)) <= 4; quasi-constant
     findRoot := func(id int) int {
         for id != ufParent[ufParent[id]] {
             ufParent[id] = ufParent[ufParent[id]]
@@ -28,7 +38,15 @@ func accountsMerge(accounts [][]string) [][]string {
         if ufParent[v1] == ufParent[v2] {
             return
         }
-        ufParent[findRoot(v1)] = findRoot(v2) 
+        v1Root, v2Root := findRoot(v1), findRoot(v2)
+        if ufSize[v1Root] <= ufSize[v2Root] {
+            ufParent[v1Root] = v2Root
+            ufSize[v2Root] += ufSize[v1Root]
+        } else {
+            ufParent[v2Root] = v1Root
+            ufSize[v1Root] += ufSize[v2Root]
+        }
+        //ufParent[findRoot(v1)] = findRoot(v2) 
     }
     
     for _, acct := range accounts {
@@ -38,6 +56,7 @@ func accountsMerge(accounts [][]string) [][]string {
         }
     }
     
+    //time: O(len(accounts) * len(acct))
     idToEmails := make(map[int][]string)
     for email, id := range emailToID {
         parId := findRoot(id)
@@ -45,7 +64,11 @@ func accountsMerge(accounts [][]string) [][]string {
     }
     
     var res [][]string
+    
+    // e = len(emails)
+    // time: O(e * nlogn)
     for _, emails := range idToEmails {
+        // intro sort go std lib sort avg & worst case time: O(nlogn)
         sort.Strings(emails)
         acct := append([]string{emailToName[emails[0]]}, emails...)  
         res = append(res, acct)
