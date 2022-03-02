18775f30c2bd999f572728c36f55c1ccde11a30c
Sergio Chairez <schairezv@gmail.com>
2022-03-01 21:33:09 -0800

    Time: 20 ms (18.30%), Space: 5.4 MB (13.02%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.go
+++ 42-trapping-rain-water/42-trapping-rain-water.go
@@ -1,29 +1,23 @@
-func min(a, b int) int {if a <= b { return a}; return b}
+
+func min(a, b int) int { if a <= b { return a }; return b}
 
 func trap(height []int) int {
-    stack := make([]int, 0) 
-    var res int
+    res := 0
+    var stack []int
+    // time: O(n)
+    // space: O(n)
     for idx := range height {
         for len(stack) > 0 && height[idx] > height[stack[len(stack)-1]] {
-            bottom := height[stack[len(stack)-1]]
+            bottom := stack[len(stack)-1]
             stack = stack[:len(stack)-1]
             if len(stack) > 0 {
-                // 
-                //w = idx - stack.top() - 1
-                // 3 -1 -1  = 1
-                w := idx - stack[len(stack)-1] - 1
-                h := min(height[stack[len(stack)-1]], height[idx])
-                res += w * (h - bottom)
+                h := min(height[idx], height[stack[len(stack)-1]]) 
+                w :=  idx - stack[len(stack)-1] - 1
+                res += (h-height[bottom])*w
+                fmt.Println(res)
             }
         }
         stack = append(stack, idx)
     }
     return res
 }
-// @ idx =3
-// [1,]; bottom = ht[2] =0
-// [0,1, 0] 2
-// [1] 2; res += 
-// height[stack.top()]
-
-// bottom = 0
