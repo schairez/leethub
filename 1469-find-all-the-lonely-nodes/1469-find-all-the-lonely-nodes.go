e2ca2887d249931e2bb89b8e8f61a457ff2b9d2d
Sergio Chairez <schairezv@gmail.com>
2022-03-02 07:54:05 -0800

    Time: 17 ms (30.51%), Space: 6.9 MB (30.51%) - LeetHub

---
+++ 1469-find-all-the-lonely-nodes/1469-find-all-the-lonely-nodes.go
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func getLonelyNodes(root *TreeNode) []int {
+    var (
+        node *TreeNode
+        res []int
+    )
+    queue := []*TreeNode{root}
+    for len(queue) != 0 {
+        node, queue = queue[0], queue[1:]
+        if node.Left != nil && node.Right != nil {
+            queue = append(queue, node.Left)
+            queue = append(queue, node.Right)
+        } else {
+            if node.Left != nil {
+                queue = append(queue, node.Left)
+                res = append(res, node.Left.Val)
+            } else if node.Right != nil {
+                queue = append(queue, node.Right)
+                res = append(res, node.Right.Val)
+            }
+        }
+    }
+    return res
+    
+}
