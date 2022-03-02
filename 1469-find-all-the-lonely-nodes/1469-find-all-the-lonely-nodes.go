7559872cff8ae2b32eeae835855851190352b827
Sergio Chairez <schairezv@gmail.com>
2022-03-02 07:56:38 -0800

    Time: 18 ms (27.12%), Space: 6.9 MB (20.34%) - LeetHub

--- 1469-find-all-the-lonely-nodes/1469-find-all-the-lonely-nodes.go
+++ 1469-find-all-the-lonely-nodes/1469-find-all-the-lonely-nodes.go
@@ -14,19 +14,22 @@ func getLonelyNodes(root *TreeNode) []int {
     queue := []*TreeNode{root}
     for len(queue) != 0 {
         node, queue = queue[0], queue[1:]
-        if node.Left != nil && node.Right != nil {
-            queue = append(queue, node.Left)
-            queue = append(queue, node.Right)
-        } else {
-            if node.Left != nil {
-                queue = append(queue, node.Left)
-                res = append(res, node.Left.Val)
-            } else if node.Right != nil {
+        if node.Left == nil {
+            if node.Right != nil {
                 queue = append(queue, node.Right)
                 res = append(res, node.Right.Val)
             }
+        } else if node.Right == nil {
+            if node.Left != nil {
+                queue = append(queue, node.Left)
+                res = append(res, node.Left.Val)
+            }
+        } else {
+            queue = append(queue, node.Left)
+            queue = append(queue, node.Right)
+            
         }
     }
-    return res
     
+    return res
 }
