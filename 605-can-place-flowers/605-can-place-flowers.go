c6e922e9d0faf510c247608f95391e4d08075009
Sergio Chairez <schairezv@gmail.com>
2022-03-03 19:23:00 -0800

    Time: 25 ms (60.05%), Space: 6.3 MB (71.74%) - LeetHub

---
+++ 605-can-place-flowers/605-can-place-flowers.go
@@ -0,0 +1,36 @@
+const (
+    empty = iota
+    planted
+)
+
+func canPlaceFlowers(flowerbed []int, n int) bool {
+    if n == 0 {
+        return true
+    }
+    if n == 0 && len(flowerbed) > 0 {
+        return false
+    }
+    numLeft := n
+    lastPlantedLoc := -1
+    for idx := 0; idx < len(flowerbed); idx++ {
+        if numLeft == 0 {
+            break
+        }
+        if flowerbed[idx] == planted {
+            continue
+        }
+        if idx != 0 && flowerbed[idx-1] == planted {
+            continue
+        } 
+        if lastPlantedLoc != -1 && lastPlantedLoc == idx-1 {
+            continue
+        } 
+        if idx+1 != len(flowerbed) && flowerbed[idx+1] == planted {
+            continue
+        }
+        lastPlantedLoc = idx
+        numLeft--
+    }
+    
+    return numLeft == 0
+}
