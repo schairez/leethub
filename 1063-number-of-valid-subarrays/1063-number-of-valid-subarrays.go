2f7b589d36eaa9994c06ab6ac7521f358bb449d6
Sergio Chairez <schairezv@gmail.com>
2022-03-05 20:24:21 -0800

    Time: 72 ms (33.33%), Space: 7.6 MB (66.67%) - LeetHub

--- 1063-number-of-valid-subarrays/1063-number-of-valid-subarrays.go
+++ 1063-number-of-valid-subarrays/1063-number-of-valid-subarrays.go
@@ -1,3 +1,19 @@
+
+/*
+         [1,4,2]
+         [0]
+         res += 1    //  [1]
+         @i=1
+         res += 2   // [4],  [1,4]
+         
+         @i=2
+          res +=2   //[2], [1,4,2]
+         
+
+mono increasing 
+
+*/
+
 func validSubarrays(nums []int) int {
     var res int
     var stack []int
