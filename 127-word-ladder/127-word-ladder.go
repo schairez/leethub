34e150b1c4bd07e5d9e72ace78edcea769aa0934
Sergio Chairez <schairezv@gmail.com>
2022-03-07 02:17:45 -0800

    Time: 68 ms (80.12%), Space: 6.1 MB (85.90%) - LeetHub

--- 127-word-ladder/127-word-ladder.go
+++ 127-word-ladder/127-word-ladder.go
@@ -1,26 +1,4 @@
 
-// get next 1 degree away candidates
-func getCandidates(word []byte, wordSet map[string]struct{}) [][]byte {
-    n := len(word)
-    var cands [][]byte
-    for i := 0; i < 26; i++ {
-        for j := 0; j < len(word); j++ {
-            origChar := word[j]
-            nextChar := byte(int('a')+i)
-            if origChar != nextChar {
-                word[j] = nextChar
-                candWord := string(word)
-                if _, exists := wordSet[candWord]; exists {
-                    tmp := make([]byte, n)
-                    copy(tmp, word)
-                    cands = append(cands, tmp)
-                }
-            }
-            word[j] = origChar
-        }
-    }
-    return cands
-}
 
 func ladderLength(beginWord string, endWord string, wordList []string) int {
     wordSet := make(map[string]struct{}, len(wordList))
@@ -50,4 +28,27 @@ func ladderLength(beginWord string, endWord string, wordList []string) int {
     }
     
     return 0 
+}
+
+// get next 1 degree away candidates
+func getCandidates(word []byte, wordSet map[string]struct{}) [][]byte {
+    n := len(word)
+    var cands [][]byte
+    for i := 0; i < 26; i++ {
+        for j := 0; j < len(word); j++ {
+            origChar := word[j]
+            nextChar := byte(int('a')+i)
+            if origChar != nextChar {
+                word[j] = nextChar
+                candWord := string(word)
+                if _, exists := wordSet[candWord]; exists {
+                    tmp := make([]byte, n)
+                    copy(tmp, word)
+                    cands = append(cands, tmp)
+                }
+            }
+            word[j] = origChar
+        }
+    }
+    return cands
 }
