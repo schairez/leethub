c0544f9e44f8916854e782393344f7b087b7325d
Sergio Chairez <schairezv@gmail.com>
2022-03-07 02:14:47 -0800

    Time: 102 ms (72.11%), Space: 6.3 MB (79.67%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.go
@@ -0,0 +1,54 @@
+
+// get next 1 degree away candidates
+func getCandidates(word []byte, wordSet map[string]struct{}) [][]byte {
+    n := len(word)
+    var cands [][]byte
+    for i := 0; i < 26; i++ {
+        for j := 0; j < len(word); j++ {
+            origChar := word[j]
+            nextChar := byte(int('a')+i)
+            if origChar != nextChar {
+                word[j] = nextChar
+                candWord := string(word)
+                if _, exists := wordSet[candWord]; exists {
+                    tmp := make([]byte, n)
+                    copy(tmp, word)
+                    cands = append(cands, tmp)
+                }
+            }
+            word[j] = origChar
+        }
+    }
+    return cands
+}
+
+func ladderLength(beginWord string, endWord string, wordList []string) int {
+    wordSet := make(map[string]struct{}, len(wordList))
+    for _, word := range wordList {
+        wordSet[word] = struct{}{}
+    }
+    if _, exists := wordSet[endWord]; !exists {
+        return 0
+    }
+    queue := [][]byte{[]byte(beginWord)}
+    var numWords int //numWords in path
+    for len(queue) != 0 {
+        numWords++
+        size := len(queue)
+        for i := 0; i < size; i++ {
+            node := queue[i]
+            if string(node) == endWord {
+                return numWords
+            }
+            cands := getCandidates(node, wordSet)
+            fmt.Println(cands)
+            for _, cand := range cands {
+                queue = append(queue, cand)
+                delete(wordSet, string(cand))
+            }
+        }
+        queue = queue[size:]
+    }
+    
+    return 0 
+}
