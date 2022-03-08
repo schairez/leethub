f4d82fca2a05f27ebe944d5ea298df66cd9168c2
Sergio Chairez <schairezv@gmail.com>
2022-03-07 16:52:45 -0800

    Time: 0 ms (100.00%), Space: 2.8 MB (92.56%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.go
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.go
@@ -1,8 +1,45 @@
 
+func intersection(nums1 []int, nums2 []int) []int {
+    binSearch := func(nums []int, target int) bool {
+        start, end := 0, len(nums)-1
+        for start <= end {
+            mid := start + (end - start) >> 1
+            if nums[mid] == target {
+                return true
+            } else if nums[mid] < target {
+                start = mid+1
+            } else {
+                end = mid-1
+            } 
+        }
+        return false
+    } 
+    
+    // sort the largest to do binsearch on this sequence
+    n1, n2 := len(nums1), len(nums2)
+    maxNumValsIntersect := n2
+    if n2 > n1 {
+        return intersection(nums2, nums1)
+    }
+    sort.Ints(nums1)
+    valsSet := make(map[int]struct{}, maxNumValsIntersect)
+    for _, val := range nums2 {
+        if isIntersectVal := binSearch(nums1, val); isIntersectVal {
+            valsSet[val] = struct{}{}
+        }
+    }
+    res := make([]int, 0, len(valsSet))
+    for key := range valsSet {
+            res = append(res, key)
+    }
+    return res
+}
+
+
 // sort and two pointers approach
 // time: O(2nlogn) + O(min(n1, n2)) ≈ O(nlogn)
 // space: O(logn) for recursive call stack in sorting std logic
-func intersection(nums1 []int, nums2 []int) []int {
+func intersectionV2(nums1 []int, nums2 []int) []int {
     n1, n2 := len(nums1), len(nums2)
     maxNumValsIntersect := n1
     if n2 < n1 { //max intersection vals
@@ -37,7 +74,7 @@ func intersection(nums1 []int, nums2 []int) []int {
 // hashset approach
 // time: O(n1 + n2) ≈ O(n)
 // space: O(max(n1, n2))
-func intersectionV2(nums1 []int, nums2 []int) []int {
+func intersectionV3(nums1 []int, nums2 []int) []int {
     hashSet := make(map[int]struct{})
     for idx := range nums1 {
         hashSet[nums1[idx]] = struct{}{}
