dec54d507225cb1fb0e216a6852bb015d231127c
Sergio Chairez <schairezv@gmail.com>
2022-03-09 15:35:52 -0800

    Time: 8 ms (79.84%), Space: 5.1 MB (24.81%) - LeetHub

---
+++ 437-path-sum-iii/437-path-sum-iii.go
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+
+
+func pathSum(root *TreeNode, targetSum int) int {
+    valsMap := make(map[int]int)
+    var (
+        totalPaths int
+        dfs        func(*TreeNode, int)
+    )
+    // postorder approach
+    dfs = func(node *TreeNode, sumSoFar int) {
+        if node == nil {
+            return
+        }
+        sumSoFar += node.Val
+        if sumSoFar == targetSum {
+            totalPaths++
+        }
+        complement := sumSoFar - targetSum
+        if tot, ok := valsMap[complement]; ok {
+            totalPaths += tot
+        }
+        valsMap[sumSoFar]++
+        dfs(node.Left, sumSoFar)
+        dfs(node.Right, sumSoFar)
+        valsMap[sumSoFar]--
+    }
+    
+    dfs(root, 0)
+    return totalPaths
+    
+}
+
+
