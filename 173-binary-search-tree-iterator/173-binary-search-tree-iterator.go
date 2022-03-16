4e2bdf7056c40bdefddde440132b280ef893401c
Sergio Chairez <schairezv@gmail.com>
2022-03-15 19:55:22 -0700

    Time: 34 ms (63.76%), Space: 10.1 MB (52.75%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.go
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.go
@@ -6,6 +6,74 @@
  *     Right *TreeNode
  * }
  */
+
+// iterator-morris version
+
+type BSTIterator struct {
+    succNode, currIterNode *TreeNode
+}
+
+
+func Constructor(root *TreeNode) BSTIterator {
+    iterator := BSTIterator{succNode: root }
+    iterator.currIterNode = iterator.findNext()
+    return iterator
+}
+
+// uses morris under the hood :P
+func (this *BSTIterator) findNext() *TreeNode {
+    var currIterNode, predNode *TreeNode
+    for this.succNode != nil {
+        if this.succNode.Left != nil {
+            predNode = this.succNode.Left
+            for predNode.Right != nil && predNode.Right != this.succNode {
+                predNode = predNode.Right
+            }
+            if predNode.Right == this.succNode {
+                predNode.Right = nil
+                currIterNode = this.succNode
+                this.succNode = this.succNode.Right
+                break
+            } else {
+                predNode.Right = this.succNode
+                this.succNode = this.succNode.Left
+            }
+        } else {
+            currIterNode = this.succNode
+            this.succNode = this.succNode.Right
+            break
+        }
+    }
+    return currIterNode
+}
+
+func (this *BSTIterator) Next() int {
+    // condition: next will be called when valid next
+    // so if not needed but here to avoid pointer dereferencing
+    /*
+    if !this.HasNext()  {
+        return -1 
+    }
+    */
+    val := this.currIterNode.Val
+    this.currIterNode = this.findNext()
+    return val
+}
+
+
+func (this *BSTIterator) HasNext() bool {
+    return this.currIterNode != nil
+}
+
+
+
+
+
+/*
+
+
+// morris with storing all node values as a preprocess
+
 type BSTIterator struct {
     nums []int
 }
@@ -16,7 +84,8 @@ func Constructor(root *TreeNode) BSTIterator {
     return BSTIterator{nums}
 }
 
-// overkill morris traversal
+// time: O(3n) ≈ O(n)
+// space: O(n) to cache all the node values into an array
 func inorderMorris(root *TreeNode) []int {
     var (
         currNode *TreeNode
@@ -59,6 +128,7 @@ func (this *BSTIterator) HasNext() bool {
     return len(this.nums) != 0
 }
 
+*/
 
 /**
  * Your BSTIterator object will be instantiated and called as such:
