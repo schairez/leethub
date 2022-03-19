079cbfabd08c81987862e356db45ab216b9426a7
Sergio Chairez <schairezv@gmail.com>
2022-03-19 01:29:23 -0700

    Time: 44 ms (5.41%), Space: 7.2 MB (7.21%) - LeetHub

---
+++ 588-design-in-memory-file-system/588-design-in-memory-file-system.go
@@ -0,0 +1,142 @@
+
+// fs --> []
+//  
+type fsNode struct {
+    key string
+    isDir bool
+    children map[string]*fsNode
+    contents *strings.Builder 
+}
+
+func (fs *fsNode) addChildNode(fsPath []string) *fsNode {
+    for _, key := range fsPath {
+        if _, exists := fs.children[key]; !exists {
+            fs.children[key] = &fsNode{key:key,
+                                       isDir: true, 
+                                       children: make(map[string]*fsNode),
+                                      }
+        }
+        fs = fs.children[key]
+        
+    }
+    return fs
+}
+
+    // [a, b, c]
+
+func (fs *fsNode) FindNode(path []string) *fsNode {
+    //derefenrce nil ptr
+    for i := 0; i < len(path); i++ {
+        fs = fs.children[path[i]]
+    }
+    return fs
+}
+
+
+type FileSystem struct {
+    root *fsNode
+}
+
+
+func Constructor() FileSystem {
+    return FileSystem{root: &fsNode{key:"/", isDir: true,
+                                    children: make(map[string]*fsNode)}}
+}
+
+// sort during query
+
+
+// /a/b/c
+// if node.children[c] == nil { file}
+
+func (this *FileSystem) Ls(path string) []string {
+    var lvlNodes []string 
+    
+    getNodes := func(currFS *fsNode) []string {
+        if currFS.isDir {
+            fmt.Println(currFS.children)
+            fmt.Println("are we")
+            for nodeNames := range currFS.children {
+                fmt.Println(nodeNames)
+                lvlNodes = append(lvlNodes, nodeNames)
+            }
+        } else {
+            return []string{currFS.key}
+        }
+        if len(lvlNodes) == 0 {
+            return lvlNodes
+        }
+        sort.Strings(lvlNodes) // O(nlogn); size of keys
+        return  lvlNodes
+    }
+    // condition: no err in path
+    pathVals := splitFilePath(path)
+    // append the subfss in this path + the filenames
+    if len(pathVals) == 0 { // if rootfs
+        lvlNodes = getNodes(this.root)
+    } else {
+        currNode := this.root.FindNode(pathVals)
+        lvlNodes = getNodes(currNode)
+    }
+    
+    return lvlNodes
+}
+
+// patricia trees
+// trie compact
+
+//////// vimium
+
+// condition:
+func (this *FileSystem) Mkdir(path string)  {
+    pathVals := splitFilePath(path)
+    this.root.addChildNode(pathVals)
+    
+}
+
+
+//           V
+// [a, b, c, d]
+func (this *FileSystem) AddContentToFile(filePath string, content string)  {
+    pathVals := splitFilePath(filePath)
+    n := len(pathVals)
+    currNode := this.root.addChildNode(pathVals[:n-1])
+    fileName := pathVals[n-1] 
+    fileNode, exists := currNode.children[fileName]
+    if !exists {
+        var sb strings.Builder
+        sb.WriteString(content)
+        currNode.children[pathVals[n-1]] = &fsNode{key: fileName, 
+                                                   isDir:false, 
+                                                   contents: &sb}
+    } else {
+        fileNode.contents.WriteString(content)
+    }
+}
+
+func splitFilePath(path string) []string {
+    if path == "/" {
+        return []string{}
+    }
+    pathVals := strings.Split(path, "/")
+    return pathVals[1:]
+}
+
+
+func (this *FileSystem) ReadContentFromFile(filePath string) string {
+    pathVals := splitFilePath(filePath)
+    currNode := this.root.addChildNode(pathVals)
+    //condition: path is valid and last val is file
+    
+    return currNode.contents.String()
+}
+
+
+/**
+ * Your FileSystem object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Ls(path);
+ * obj.Mkfs(path);
+ * obj.AddContentToFile(filePath,content);
+ * param_4 := obj.ReadContentFromFile(filePath);
+ */
