11d584d0d222d207721c260def611f8aea22e70c
Sergio Chairez <schairezv@gmail.com>
2022-03-20 00:31:46 -0700

    Time: 0 ms (100.00%), Space: 5.6 MB (33.33%) - LeetHub

--- 818-race-car/818-race-car.go
+++ 818-race-car/818-race-car.go
@@ -1,7 +1,8 @@
+
 func min(a, b int) int {if a <=b { return a}; return b}
 
+
 func racecar(target int) int {
-    //maxInt32 := (1 << 31) - 1
     type nodeData struct { position, velocity, steps int }
     var queue []nodeData
     var (
@@ -10,14 +11,6 @@ func racecar(target int) int {
     )
     sourceNode = nodeData{0, 1, 0}
     queue = append(queue, sourceNode)
-    //res := maxInt32
-    
-    // 
-    /////  {1, 2, 1}
-    //  {3, 4, 2}
-    //    {3, -1, 3}
-    ///////// {3, 1, 4} // AARRA
-    ////////  {4, 2, 5}
     for len(queue) != 0 {
         currNode, queue = queue[0], queue[1:]
         if currNode.position == target {
@@ -27,14 +20,12 @@ func racecar(target int) int {
         pos, vel, steps := currNode.position, currNode.velocity, currNode.steps
         queue = append(queue, nodeData{pos+vel, vel*2, steps+1})
         
-        if (pos + vel > target && vel > 0 ) || (pos > target && vel > 0) {
+        if (pos + vel > target && vel > 0 ) {
             queue = append(queue, nodeData{pos, -1, steps+1})
-        } else if (pos + vel < target  && vel < 0) || (pos < target && vel < 0) {
+        } else if (pos + vel < target  && vel < 0) {
             queue = append(queue, nodeData{pos, 1, steps+1})
         }
-        
     }
-    
     return -1
-    //return res
 }
+
