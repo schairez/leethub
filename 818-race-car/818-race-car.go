d55e1ad10a2baf3f7493e429c417899576ed4ef4
Sergio Chairez <schairezv@gmail.com>
2022-03-23 02:59:25 -0700

    Time: 0 ms (100.00%), Space: 7.4 MB (70.13%) - LeetHub

--- 818-race-car/818-race-car.go
+++ 818-race-car/818-race-car.go
@@ -1,31 +1,31 @@
-func racecar(target int) int {
-    type pair struct {pos, vel int}
-    var queue []pair
-    var (
-        currNode pair
-        srcNode pair
-    )
-    srcNode = pair{0,1}
-    queue = append(queue, srcNode)
+class Solution {
+public:
     
-    dist := 0 
-    for len(queue) != 0 {
-        //n := len(queue)
-        for n := len(queue); n != 0; n-- {
-            currNode, queue = queue[0], queue[1:]   
-            if currNode.pos == target {
-                return dist
+    int racecar(long int target) {
+        queue<pair<int, long int>> q;
+        q.push({0, 1});
+        long int depth = 0;
+        
+        while (true) {
+            //vector<pair<long int, long int>> next_q;
+            int n = q.size(); 
+            for (int idx = 0; idx < n; idx++) {
+                pair<int, long int> node = q.front();
+                q.pop();
+                int pos = node.first;
+                long int speed = node.second;
+                if (pos == target) {
+                    return depth;
+                }
+                q.push({ pos + speed, 2 * speed });
+                if (pos + speed > target && speed > 0) {
+                    q.push({ pos, -1 });
+                } else if (pos + speed < target && speed < 0) {
+                    q.push({ pos, 1 });
+                }
             }
-            pos, vel := currNode.pos, currNode.vel
-            queue = append(queue, pair{pos+vel, vel*2})
-            if pos + vel > target && vel > 0 {
-                queue = append(queue, pair{pos, -1})
-            } else if pos + vel < target && vel < 0 {
-                queue = append(queue, pair{pos, 1})
-            }
+            ++depth;
         }
-        dist++
+        return -1;
     }
-    
-    return -1
-}
+};
