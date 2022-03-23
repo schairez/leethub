b9923ae0d85748379feb7e36afa78207f429932e
Sergio Chairez <schairezv@gmail.com>
2022-03-23 03:49:02 -0700

    Time: 4 ms (57.89%), Space: 3.2 MB (57.89%) - LeetHub

--- 818-race-car/818-race-car.go
+++ 818-race-car/818-race-car.go
@@ -1,31 +1,36 @@
-class Solution {
-public:
+
+func racecar(target int) int {
     
-    int racecar(long int target) {
-        queue<pair<int, long int>> q;
-        q.push({0, 1});
-        long int depth = 0;
-        
-        while (true) {
-            //vector<pair<long int, long int>> next_q;
-            int n = q.size(); 
-            for (int idx = 0; idx < n; idx++) {
-                pair<int, long int> node = q.front();
-                q.pop();
-                int pos = node.first;
-                long int speed = node.second;
-                if (pos == target) {
-                    return depth;
-                }
-                q.push({ pos + speed, 2 * speed });
-                if (pos + speed > target && speed > 0) {
-                    q.push({ pos, -1 });
-                } else if (pos + speed < target && speed < 0) {
-                    q.push({ pos, 1 });
-                }
+    type pair struct {pos, vel int}
+    var queue []pair
+    var (
+        currNode pair
+        srcNode pair
+    )
+    srcNode = pair{0,1}
+    queue = append(queue, srcNode)
+    
+    dist := 0 
+    for len(queue) != 0 {
+        // 2 
+        for n := len(queue); n != 0; n-- {
+            currNode, queue = queue[0], queue[1:]   
+            if currNode.pos == target {
+                return dist
             }
-            ++depth;
+            pos, vel := currNode.pos, currNode.vel
+            if pos + vel <= 2 * target && pos + vel > 0 {
+                queue = append(queue, pair{pos+vel, vel*2})
+            }
+            // if we overshoot
+            if pos + vel > target && vel > 0 {
+                queue = append(queue, pair{pos, -1})
+            } else if pos + vel < target && vel < 0 { // if we get further from t and neg vel
+                queue = append(queue, pair{pos, 1})
+            }
         }
-        return -1;
+        dist++
     }
-};
+    
+    return -1
+}
