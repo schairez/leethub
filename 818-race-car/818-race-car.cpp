2f4d88561d467a3e65c00241e54669595d4dcd13
Sergio Chairez <schairezv@gmail.com>
2022-03-23 03:14:21 -0700

    Time: 6 ms (84.23%), Space: 6.3 MB (91.28%) - LeetHub

--- 818-race-car/818-race-car.cpp
+++ 818-race-car/818-race-car.cpp
@@ -1,21 +1,21 @@
-
 class Solution {
 public:
     int racecar(long int target) {
         queue<pair<int, int>> q;
         q.push({0, 1});
         int depth = 0;
-        while (!q.empty()) {
+        while (true) {
+            //vector<pair<long int, long int>> next_q;
             int n = q.size(); 
             for (int idx = 0; idx < n; idx++) {
                 pair<int, int> node = q.front();
                 q.pop();
                 int pos = node.first;
-                int speed = node.second;
+                long int speed = node.second;
                 if (pos == target) {
                     return depth;
                 }
-                if (abs(pos) > 2 * target) continue;
+                if (pos > 2 * target) continue;
                 
                 q.push({ pos + speed, 2 * speed });
                 if (pos + speed > target && speed > 0) {
