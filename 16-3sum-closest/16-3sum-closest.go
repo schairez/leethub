1b662a5b613248dab34bc8b7130623ecac327d09
Sergio Chairez <schairezv@gmail.com>
2022-03-24 02:52:28 -0700

    Time: 4 ms (95.45%), Space: 2.8 MB (73.91%) - LeetHub

---
+++ 16-3sum-closest/16-3sum-closest.go
@@ -0,0 +1,71 @@
+
+
+// [-1,2,1,-4]
+// [-4, -1, 1, 2]
+//   ^         ^
+
+func min(a, b int) int {
+    if a <= b {
+        return a
+    }
+    return b
+}
+
+func abs(x int) int {
+    if x < 0 {
+        return -x
+    }
+    return x
+}
+
+func threeSumClosest(nums []int, target int) int {
+    const maxInt32 = (1 << 31) - 1
+    n := len(nums)
+    if n < 3 {
+        return 0
+    }
+    res := 0
+    minDist := maxInt32
+    sort.Ints(nums)
+    for thirdIdx := n-1; thirdIdx > 1; thirdIdx-- {
+        thirdVal := nums[thirdIdx]
+        start, end := 0, thirdIdx-1
+        for start < end {
+            threeSum := nums[start] + nums[end] + thirdVal
+            candDist := abs(target - threeSum)
+            if candDist == 0 {
+                return threeSum
+            }
+            if candDist < minDist {
+               minDist = candDist
+                res = threeSum
+            } 
+            if threeSum < target {
+                start++
+            } else {
+                end--
+            }
+        }
+    }
+    return res
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
