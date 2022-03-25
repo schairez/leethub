8c00f5d7c26df6fa4fe7ab9e3d99a56f38ae5e23
Sergio Chairez <schairezv@gmail.com>
2022-03-25 01:10:45 -0700

    Time: 15 ms (37.77%), Space: 3.5 MB (56.66%) - LeetHub

---
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.go
@@ -0,0 +1,39 @@
+
+//import "math/rand"
+
+func init() {
+    rand.Seed(time.Now().UnixNano())
+}
+
+
+// iterative quick select approach
+
+func findKthLargest(nums []int, k int) int {
+    n := len(nums)
+    rand.Shuffle(n, func(i, j int) {
+        nums[i], nums[j] = nums[j], nums[i]
+    })
+    
+    kIdx := n - k
+    for l, r := 0, n-1; l < r; {
+        pivotElem := nums[l]
+        start, end := l, r+1
+        for {
+            for start++; start < r && nums[start] <= pivotElem; start++ {}
+            for end--; end > l && pivotElem <= nums[end]; end-- {}
+            if start >= end {
+                break
+            }
+            nums[start], nums[end] = nums[end], nums[start]
+        }
+        nums[l], nums[end] = nums[end], pivotElem
+        if end == kIdx {
+            break
+        } else if end < kIdx {
+            l = end + 1
+        } else {
+            r = end - 1
+        }
+    }
+    return nums[kIdx]
+}
