9d4a839bc569663ae7b2ffd803e7427c0540093f
Sergio Chairez <schairezv@gmail.com>
2022-03-24 19:04:42 -0700

    Time: 6 ms (58.64%), Space: 7.2 MB (30.89%) - LeetHub

---
+++ 227-basic-calculator-ii/227-basic-calculator-ii.go
@@ -0,0 +1,62 @@
+
+
+func isNumeric(b byte) bool {
+    if v := int(b - '0'); v >= 0 && v <= 9 {
+        return true
+    }
+    return false
+}
+
+func isWhitespace(b byte) bool {
+    return b == ' '
+}
+
+
+func calculate(s string) int {
+    var (
+        stack    []int
+        topElem  int
+        num      int
+        operator byte 
+        //currChar byte
+    )
+    operator = '+'
+    n := len(s)
+    //i := 0
+    //for  i < n {
+    for i := 0; i < n; i++ {
+        //currChar = s[i]
+        for i < n && isWhitespace(s[i]) {
+            i++
+        }
+        if i < n && isNumeric(s[i]) {
+            num = 0
+            for i < n  && isNumeric(s[i]) {
+                num = num * 10 + int(s[i] - '0')
+                i++
+            }
+            i--
+            switch operator {
+            case '+':
+                stack = append(stack, num)
+            case '-':
+                stack = append(stack, -num)
+            case '*':
+                topElem, stack = stack[len(stack)-1], stack[:len(stack)-1]
+                stack = append(stack, topElem * num )
+            case '/':
+                topElem, stack = stack[len(stack)-1], stack[:len(stack)-1]
+                stack = append(stack, topElem / num )
+            }
+        } else if i < n && !isWhitespace(s[i]) {
+            operator = s[i]
+        }
+    }
+    res := 0
+    for len(stack) != 0 {
+        topElem, stack = stack[len(stack)-1], stack[:len(stack)-1]
+        res += topElem 
+    }
+    return res
+}
+    
