f2b113f3d81c88f97b6ba3b19ebe6c8a8f1cabdb
Sergio Chairez <schairezv@gmail.com>
2022-03-25 21:18:46 -0700

    Time: 286 ms (20.83%), Space: 11.3 MB (29.17%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.go
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.go
@@ -1,21 +1,30 @@
+
+    
+// 1 <= len <= 10000
 func canSeePersonsCount(heights []int) []int {
-    n := len(heights)
-    var stack []int
-    res := make([]int, n)
-    for i := range heights {
-        for {
-            if len(stack) == 0 ||
-            heights[stack[len(stack)-1]] > heights[i] {
-                break
-            } 
-            idx := stack[len(stack)-1]
-            stack = stack[:len(stack)-1]
-            res[idx]++
-        }
-        if len(stack) > 0 {
-            res[stack[len(stack)-1]]++
-        }
-        stack = append(stack, i)
-    }
-    return res
+	n := len(heights)
+	if n == 1 {
+		return []int{0}
+	}
+	// mono decr
+	var topIdx int
+	var stack []int
+	res := make([]int, n)
+	/// 10, 6, when at 8
+	for i := range heights {
+		for {
+			if len(stack) == 0 || heights[i] < heights[stack[len(stack)-1]] {
+				break
+			}
+			topIdx, stack = stack[len(stack)-1], stack[:len(stack)-1]
+			res[topIdx]++
+			// pop elem
+			// incr
+		}
+		if len(stack) != 0 {
+			res[stack[len(stack)-1]]++
+		}
+		stack = append(stack, i)
+	}
+	return res
 }
