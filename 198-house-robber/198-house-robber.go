759b123fec0ed8d84250d6874b093fb7aa01df57
Sergio Chairez <schairezv@gmail.com>
2022-03-27 00:15:52 -0700

    Time: 3 ms (30.36%), Space: 2 MB (37.61%) - LeetHub

---
+++ 198-house-robber/198-house-robber.go
@@ -0,0 +1,15 @@
+func max(a, b int) int { if a >= b { return a}; return b}
+func rob(nums []int) int {
+    n := len(nums)
+    if n == 1 {
+        return nums[0]
+    }
+    dp := make([]int, n+1)
+    dp[0] = 0
+    dp[1] = nums[0]
+    for i := 2; i <= n; i++ {
+        dp[i] = max(dp[i-1], dp[i-2] + nums[i-1])  
+    }
+    
+    return dp[n]
+}
