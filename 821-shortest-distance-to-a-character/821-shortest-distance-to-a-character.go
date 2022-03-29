ac7925022eeea65f28d3ef720b767ab8d0b28b80
Sergio Chairez <schairezv@gmail.com>
2022-03-28 23:37:52 -0700

    Time: 3 ms (47.92%), Space: 2.4 MB (43.75%) - LeetHub

--- 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.go
+++ 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.go
@@ -18,12 +18,10 @@ func shortestToChar(s string, c byte) []int {
             }
         } else {
             charIdx = end
-            //found = true
             if !found { found = true } 
             start := end -1
             for start >= 0 && s[start] != c {
                 cand := abs(start - charIdx)
-                fmt.Println(start, cand)
                 if res[start] != 0 && res[start] < cand {
                     break
                 }
@@ -38,40 +36,3 @@ func shortestToChar(s string, c byte) []int {
     return res
     
 }
-
-
-/*
-
-    start, end := -1, 0
-
-
-    for end < n {
-        if s[end] == c {
-            charIdx = end
-            for {
-                start++
-                if start == n || s[start] == c {
-                    break
-                }
-                fmt.Println("yo", start)
-                res[start] = abs(start - charIdx)
-                fmt.Println(res)
-                //start++
-            }
-        } else {
-            start++
-            if start >= n || start == -1 {
-                break
-            }
-            res[start] = abs(start - charIdx)
-            start++
-        }
-        /*
-        if start < n {
-        } else {
-            start = end+1
-        }
-        end++
-    }
-        */
-
