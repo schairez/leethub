1e745ef18656699da2ed0b0ee6abafc635ef0317
Sergio Chairez <schairezv@gmail.com>
2022-04-12 16:11:32 -0700

    Time: 44 ms (83.99%), Space: 14.1 MB (61.02%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,19 @@
+# time: O(n!)
+# space: O(n!)
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        res = []
+        def helper(idx: int):
+            nonlocal res
+            nonlocal n
+            if idx == n:
+                res.append(nums[:])
+            for i in range(idx, n):
+                nums[idx], nums[i] = nums[i], nums[idx]
+                helper(idx+1)
+                nums[idx], nums[i] = nums[i], nums[idx]
+            
+        helper(0)
+        return res
