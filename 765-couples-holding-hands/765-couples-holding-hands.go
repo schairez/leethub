4b89283bb59b97003058b130218e267a13645ee9
Sergio Chairez <schairezv@gmail.com>
2022-04-15 17:56:09 -0700

    Time: 2 ms (20.00%), Space: 1.9 MB (100.00%) - LeetHub

---
+++ 765-couples-holding-hands/765-couples-holding-hands.go
@@ -0,0 +1,52 @@
+
+//
+//
+
+func abs(x int) int {
+    if x < 0 {
+        return -x
+    }
+    return x
+}
+
+// TODO: not deterministic
+func isCouple(num1, num2 int) bool {
+    if num1 % 2 == 0 && num2 % 2 == 0 {
+        return false
+    }
+    if num1 % 2 == 1 && num2 % 2 == 1 {
+        return false
+    }
+    if num1 % 2 == 0 && num1 + 1 != num2 {
+        return false
+    } else if num2 % 2 == 0 && num2 + 1 != num1 {
+        return false
+    }
+    return true
+}
+
+func minSwapsCouples(row []int) int {
+    n := len(row)
+    if n == 2 {
+        return 0
+    }
+    res := 0
+    i, j, k := 0, 1, 2
+    for i < n-2 {
+        if isCouple(row[i], row[j]) {
+            i += 2
+            j = i + 1
+            k = j + 1
+        } else {
+            for k < n && !isCouple(row[i], row[k]) {
+                k++
+            }
+            row[j], row[k] = row[k], row[j]
+            i += 2
+            j = i + 1
+            k = j + 1
+            res++
+        }
+    }
+    return res
+}
