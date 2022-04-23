8a81601a1d385e50ec95371e335d2e53a20550d5
Sergio Chairez <schairezv@gmail.com>
2022-04-23 15:42:36 -0700

    Time: 22 ms (70.15%), Space: 6.6 MB (73.06%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.go
+++ 547-number-of-provinces/547-number-of-provinces.go
@@ -1,44 +1,46 @@
+
 func findCircleNum(isConnected [][]int) int {
     n := len(isConnected)
-    ufPar := make([]int, n)
-    ufSize := make([]int, n)
-    for x := range ufSize {
-        ufSize[x] = 1
-        ufPar[x] = x
+    numProvinces := n
+    parent := make([]int, n)
+    sze := make([]int, n)
+    for x := range sze {
+        sze[x] = 1
+        parent[x] = x
     }
-    numForests := n
+    // path compression
+    // inv ackerman O(a(n)) <= 4
     findRoot := func(x int) int {
-        for x != ufPar[x] {
-            ufPar[x] = ufPar[ufPar[x]]
-            x = ufPar[x]
+        for x != parent[x] {
+            parent[x] = parent[parent[x]]
+            x = parent[x]
         }
         return x
     }
+    // union by size with path compression
     union := func(w, v int) bool {
-        rootW, rootV := findRoot(w), findRoot(v)
-        if rootW == rootV {
+        wRoot := findRoot(w)
+        vRoot := findRoot(v)
+        if wRoot == vRoot {
             return false
         }
-        wSze, vSze := ufSize[rootW], ufSize[rootV]
-        if wSze <= vSze {
-            ufSize[vSze] += wSze
-            ufPar[rootW] = rootV
-        } else {
-            ufSize[wSze] += vSze
-            ufPar[rootV] = rootW
+        // small into big
+        if sze[wRoot] <= sze[vRoot] {
+            sze[vRoot] += sze[wRoot]
+            parent[wRoot] = vRoot
+        } else { // wRoot > vRoot
+            sze[wRoot] += sze[vRoot]
+            parent[vRoot] = wRoot
         }
         return true
     }
-    
     for x := 0; x < n; x++ {
         for y := 0; y < n; y++ {
             if isConnected[x][y] == 1 && union(x, y) {
-                numForests--
+                numProvinces--
             }
         }
     }
-    
-    
-    return numForests
+    return numProvinces
     
 }
