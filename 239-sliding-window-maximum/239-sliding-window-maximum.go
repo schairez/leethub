da0d592b2b44dd2f7690347111fc7311bb831f71
Sergio Chairez <schairezv@gmail.com>
2022-04-23 21:46:42 -0700

    Time: 216 ms (87.90%), Space: 9.3 MB (87.19%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.go
+++ 239-sliding-window-maximum/239-sliding-window-maximum.go
@@ -1,8 +1,13 @@
+// 239. Sliding Window Maximum
+// mono decr queue approach
+// time: O(n) 
+// space: O(k) for the deque + O(n-k+1) for res
+
 func maxSlidingWindow(nums []int, k int) []int {
     n := len(nums)
     numWin := n - k + 1
     res := make([]int, 0, numWin)
-    var deque []int
+    deque := make([]int, 0, k)
     
     for i := 0; i < n; i++ {
         if len(deque) != 0 && deque[0] == i-k {
