e88ffcbd16c62ec43fbe49074c7eafe4d508c8ea
Sergio Chairez <schairezv@gmail.com>
2022-04-27 00:53:14 -0700

    Time: 53 ms (54.95%), Space: 7.3 MB (71.17%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.go
@@ -0,0 +1,28 @@
+func canReach(arr []int, start int) bool {
+    n := len(arr)
+    visited := make([]bool, n)
+    var (
+        node  int
+        queue []int
+    )
+    visited[start] = true
+    queue = append(queue, start)
+    for len(queue) != 0 {
+        for currLen := len(queue); currLen != 0; currLen-- {
+            node, queue = queue[0], queue[1:]
+            if arr[node] == 0 {
+                return true
+            }
+            // at index i, you can jump to i + arr[i] or i - arr[i
+            if nei := node + arr[node]; nei < n && !visited[nei] {
+                queue = append(queue, nei)
+                visited[nei] = true
+            }
+            if nei := node - arr[node]; nei >= 0 && !visited[nei] {
+                queue = append(queue, nei)
+                visited[nei] = true
+            }
+        }
+    }
+    return false
+}
