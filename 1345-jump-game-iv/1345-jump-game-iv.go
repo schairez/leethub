1a607875e8f502550679f945ec010cc24afcb56b
Sergio Chairez <schairezv@gmail.com>
2022-04-27 01:28:17 -0700

    Time: 119 ms (83.72%), Space: 17.8 MB (9.30%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.go
+++ 1345-jump-game-iv/1345-jump-game-iv.go
@@ -1,3 +1,54 @@
+
+func minJumps(arr []int) int {
+    n := len(arr)
+    if n == 1 {
+        return 0
+    }
+    dst := n-1
+    jumpIndexes := make(map[int][]int, n)
+    for i, v := range arr {
+        if _, ok := jumpIndexes[v]; !ok {
+            jumpIndexes[v] = make([]int, 0)
+        }
+        jumpIndexes[v] = append(jumpIndexes[v], i)
+    }
+    queue := make([]int, 0, n)
+    queue = append(queue, 0)
+    visited := make([]bool, n) // for visiting nodes
+    visited[0] = true
+    lvl := 0
+    var node int
+    for len(queue) != 0 {
+        for currLen := len(queue); currLen != 0; currLen-- {
+            node, queue = queue[0], queue[1:]
+            if node == dst {
+                return lvl 
+            }
+            if nei := node + 1; nei < n && !visited[nei] {
+                queue = append(queue, nei)
+                visited[nei] = true
+            }
+            if nei := node - 1; nei >= 0 && !visited[nei] {
+                queue = append(queue, nei)
+                visited[nei] = true
+            }
+            neighbors, exists := jumpIndexes[arr[node]] 
+            if exists {
+                for _, nei := range neighbors {
+                    if !visited[nei] {
+                        queue = append(queue, nei)
+                        visited[nei] = true
+                    }
+                }
+                delete(jumpIndexes, arr[node])
+            }
+        }
+        lvl++
+    }
+    return -1
+}
+
+/*
 func minJumps(arr []int) int {
     n := len(arr)
     if n == 1 {
@@ -49,3 +100,4 @@ func minJumps(arr []int) int {
     }
     return -1
 }
+*/
