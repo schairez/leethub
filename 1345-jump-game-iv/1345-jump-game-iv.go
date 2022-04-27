20c033980788b20580be7ff70823775fc743dce8
Sergio Chairez <schairezv@gmail.com>
2022-04-27 01:48:17 -0700

    Time: 109 ms (90.70%), Space: 11.9 MB (81.40%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.go
+++ 1345-jump-game-iv/1345-jump-game-iv.go
@@ -1,44 +1,9 @@
-func minJumps(arr []int) int {
-    eqi := make(map[int][]int)
-    vis := make(map[int]bool)
-    n := len(arr)
-    dp := make([]int, n)
-    for i, v := range arr{
-        eqi[v] = append(eqi[v], i)
-        dp[i] = 1e9
-    }
-    queue := make([]int, 1, n)
-    queue[0] = 0
-    dp[0] = 0
-    for len(queue) != 0{
-        i := queue[0]
-        queue = queue[1:]
-        if !vis[arr[i]]{
-            for _, j := range eqi[arr[i]]{
-                if dp[j] == 1e9{
-                    dp[j] = dp[i] + 1
-                    queue = append(queue, j)
-                }
-            }
-            vis[arr[i]] = true
-        }
-        if i + 1 < n && dp[i + 1] == 1e9{
-            dp[i + 1] = dp[i] + 1
-            queue = append(queue, i + 1)
-        }
-        if i - 1 > 0 && dp[i - 1] == 1e9{
-            dp[i - 1] = dp[i] + 1
-            queue = append(queue, i - 1)
-        }
-    }
-    return dp[n - 1]
-}
 
-
+// 1345. Jump Game IV
 // time: O(n)
 // space: O(n)
 
-func minJumps2(arr []int) int {
+func minJumps(arr []int) int {
     n := len(arr)
     if n == 1 {
         return 0
