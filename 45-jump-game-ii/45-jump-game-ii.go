62e3af74b74f109ca89fd2d7be61072330836440
Sergio Chairez <schairezv@gmail.com>
2022-04-26 19:52:16 -0700

    Time: 292 ms (10.25%), Space: 7.3 MB (5.74%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.go
+++ 45-jump-game-ii/45-jump-game-ii.go
@@ -1,187 +1,28 @@
-//time: O(n)
-//space: O(1)
-
-func max(a, b int) int {if a >= b { return a}; return b}
-
-func jump(nums []int) int {
-    dst := len(nums) - 1
-    //furthestIdx in current jump
-    var numSteps, currJumpEnd, farthestIdx int
-    for i := range nums[:len(nums)-1] {
-        farthestIdx = max(farthestIdx, i + nums[i])
-        if i == currJumpEnd {
-            numSteps++
-            if farthestIdx >= dst {
-                return numSteps
-            }
-            currJumpEnd = farthestIdx
-        }
-    }
-    return numSteps
-}
-
-
-
-
-
-
-
-
-
-/*
-
-func min(a, b int) int {
-    if a < b {
-        return a
-    } 
-    return b
-}
-
-//condition: len(nums) >=1 ...
-func jump(nums []int) int {
-    n := len(nums)
-    if n == 1 {
-        return 0 
-    }
-    numSteps := make([]int, n)
-    visited := make([]bool, n)
-    lastIdx := n-1
-    queue := []int{0}
-    numSteps[0] = 0
-    visited[0] = true
-    var node int
-    for len(queue) != 0 {
-        node, queue = queue[0], queue[1:]
-        numJumps := nums[node]
-        nextIdx := min(numJumps+node, lastIdx)
-        for nextIdx > node {
-            if !visited[nextIdx] {
-                visited[nextIdx] = true
-                numSteps[nextIdx] = numSteps[node] + 1
-                if nextIdx == lastIdx {
-                    return numSteps[lastIdx]
-                }
-                queue = append(queue, nextIdx)
-            }
-            nextIdx--
-        }
-    } 
-    return numSteps[lastIdx]
-}
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-
-// explore based on number of steps taken
-// shortest path approach is a marker for BFS
-
-
 func min(a, b int) int { if a <= b { return a}; return b}
 
-
-//BFS w/o queue approach
-//time: O(n) 
-//space: O(n)
-
+// time: O(n^2) space: O(n)
 func jump(nums []int) int {
     n := len(nums)
-    if n == 1 {
-        return 0
+    memo := make([]int, n)
+    for i := range memo {
+        memo[i] = 10_000
     }
-    distToNode := make([]int, n) //dp array
-    lastIdx := len(nums) - 1
-    visited := make([]bool, n)
-    nextIdxToJump := 0
-    visited[nextIdxToJump] = true
-    distToNode[nextIdxToJump] = 0
-    for idx, val := range nums {
-        nextIdxToJump = min(val + idx, lastIdx)
-        for nextIdxToJump != idx {
-            if !visited[nextIdxToJump] {
-                if nextIdxToJump == lastIdx {
-                    return distToNode[idx] + 1
-                }
-                distToNode[nextIdxToJump] = distToNode[idx] + 1
-                visited[nextIdxToJump] = true
-            } 
-            nextIdxToJump--
+    var dfs func(int) int
+    dfs = func(idx int) int {
+        if idx >= n - 1 {
+            return 0
         }
-    }
-    return distToNode[lastIdx]
-}
-
-*/
-    
-/*
-//BFS with queue approach
-//time: O(n) 
-//space: O(n)
-func jump(nums []int) int {
-    n := len(nums)
-    if n == 1 {
-        return 0
-    }
-    lastIdx := len(nums) - 1
-    currNode, numSteps := 0, 0
-    visited := make([]bool, n)
-    visited[0] = true
-    //enqueue startNode @ idx=0
-    queue := []int{0}
-    for len(queue) != 0 {
-        numNodesAtLvl := len(queue)
-        for numNodesAtLvl != 0 {
-            currNode, queue = queue[0], queue[1:]
-            nextNumJumps := min(nums[currNode], lastIdx-currNode)
-            for nextNumJumps != 0 {
-                nextLvlNode := currNode + nextNumJumps
-                if nextLvlNode == lastIdx {
-                    return numSteps+1
-                }
-                if !visited[nextLvlNode] {
-                    visited[nextLvlNode] = true
-                    queue = append(queue, nextLvlNode)
-                }
-                nextNumJumps--
-            }
-            numNodesAtLvl--
+        if memo[idx] != 10_000 {
+            return memo[idx]
         }
-        numSteps++
+        minJumps := 10_000
+        nextIdx, furthestIdx := idx + 1, idx + nums[idx]
+        for nextIdx <= furthestIdx {
+            minJumps = min(minJumps, 1 + dfs(nextIdx)) 
+            nextIdx++
+        }
+        memo[idx] = minJumps
+        return memo[idx]
     }
-    return numSteps+1
+    return dfs(0)
 }
-*/
