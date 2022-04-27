b0ad4677821d1648da53b3e1fff3b70f7770ac66
Sergio Chairez <schairezv@gmail.com>
2022-04-27 00:43:53 -0700

    Time: 16 ms (71.52%), Space: 6 MB (75.20%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.go
+++ 45-jump-game-ii/45-jump-game-ii.go
@@ -1,7 +1,64 @@
+
+func max(a, b int) int { if a >= b { return a}; return b}
+
+// 45. Jump Game II
+// time: O(n) space: O(1)
+// condition: we can reach lastIdx
+func jump(nums []int) int {
+    n := len(nums)
+    dst := n-1
+    numJumps := 0
+    currLvlEnd, furthestIdx := 0, 0
+    for i := 0; i < n-1; i++ {
+        furthestIdx = max(furthestIdx, i + nums[i])
+        if i == currLvlEnd {
+            numJumps++
+            if furthestIdx >= dst {
+                return numJumps
+            }
+            currLvlEnd = furthestIdx
+        }
+    }
+    return numJumps
+}
+
+
+
+
+func jumpBFS(nums []int) int {
+    lvl := 0
+    n := len(nums)
+    var (
+        node  int
+        queue []int
+    )
+    visited := make([]bool, n)
+    queue = append(queue, 0)
+    for len(queue) != 0 {
+        for currLen := len(queue); currLen != 0; currLen-- {
+            node, queue = queue[0], queue[1:]
+            if node == n-1 {
+                return lvl
+            }
+            furthestIdx := min(node + nums[node], n-1)
+            for nextIdx := 1 + node; nextIdx <= furthestIdx; nextIdx++ {
+                if !visited[nextIdx] {
+                    queue = append(queue, nextIdx)
+                    visited[nextIdx] = true
+                }
+            }
+        }
+        lvl++
+    }
+    return -1
+}
+
 func min(a, b int) int { if a <= b { return a}; return b}
 
+
+// top down dfs memo approach
 // time: O(n^2) space: O(n)
-func jump(nums []int) int {
+func jumpDFS(nums []int) int {
     n := len(nums)
     memo := make([]int, n)
     for i := range memo {
