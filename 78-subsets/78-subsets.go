1dbbb9bd284796b2cf7fbc3a776600e330993943
Sergio Chairez <schairezv@gmail.com>
2022-04-28 16:00:29 -0700

    Time: 1 ms (53.98%), Space: 2.2 MB (98.82%) - LeetHub

---
+++ 78-subsets/78-subsets.go
@@ -0,0 +1,26 @@
+
+// time: O(n*2^n)
+// space: O(n*2^n)
+
+func subsets(nums []int) [][]int {
+    n := len(nums)
+    var (
+        res [][]int
+        dfs func(int)
+    )
+    subset := make([]int, 0, n)
+    
+    dfs = func(idx int) {
+        res = append(res, append([]int{}, subset...))
+        if idx == n {
+            return
+        }
+        for i := idx; i < n; i++ {
+            subset = append(subset, nums[i])
+            dfs(i+1)
+            subset = subset[:len(subset)-1]
+        }
+    }
+    dfs(0)
+    return res
+}
