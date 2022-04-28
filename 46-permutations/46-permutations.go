475ce19e0e8b69ab8a1a0fd4ec49a2576331535d
Sergio Chairez <schairezv@gmail.com>
2022-04-28 15:25:12 -0700

    Time: 0 ms (100.00%), Space: 2.5 MB (88.80%) - LeetHub

--- 46-permutations/46-permutations.go
+++ 46-permutations/46-permutations.go
@@ -1,20 +1,22 @@
 func permute(nums []int) [][]int {
     n := len(nums)
     var (
-        dfs func(int)
         res [][]int
+        dfs func(int)
     )
+    
     dfs = func(idx int) {
-        if idx == n {
+        if idx == n-1 {
             res = append(res, append([]int{}, nums...))
             return
         }
         for i := idx; i < n; i++ {
-            nums[idx], nums[i] = nums[i], nums[idx]
+            nums[i], nums[idx] = nums[idx], nums[i]
             dfs(idx+1)
-            nums[idx], nums[i] = nums[i], nums[idx]
+            nums[i], nums[idx] = nums[idx], nums[i]
         }
     }
     dfs(0)
+    
     return res
 }
