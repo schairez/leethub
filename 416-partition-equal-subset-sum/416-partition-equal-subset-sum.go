db273aad499fbba20c8746135beab8105a97dc03
Sergio Chairez <schairezv@gmail.com>
2022-05-04 12:50:32 -0700

    Time: 2367 ms (5.09%), Space: 29.4 MB (6.48%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.go
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.go
@@ -1,26 +1,36 @@
-
-
-
 func canPartition(nums []int) bool {
     n := len(nums)
-    totalSum := 0
+    sumTotal := 0
     for i := range nums {
-        totalSum += nums[i]
+        sumTotal += nums[i]
     }
-    if totalSum % 2 != 0 {
+    if sumTotal % 2 == 1 {
         return false
     }
-    partTarget := totalSum / 2
-    
-    dp := make([]bool, partTarget + 1)
-    dp[0] = true
-    
-    // 11 -  _ = -values is err
-    for i := 0; i < n; i++ {
-        for j := partTarget; j >= nums[i]; j-- {
-            //t or f
-            dp[j] = dp[j] || dp[j-nums[i]]
+    type pair struct {idx, total int}
+    memo := make(map[pair]bool)
+    var (
+        dfs func(int, int) bool
+    )
+    // target is half
+    target := sumTotal >> 1
+    dfs = func(idx, total int) bool {
+        if total == 0 {
+            return true
         }
+        if total < 0 || idx >= n {
+            return false
+        }
+        if isPossible, exists := memo[pair{idx, total}]; exists { 
+            return isPossible
+        }
+        incl := dfs(idx+1, total - nums[idx])
+        excl := dfs(idx+1, total)
+        
+        memo[pair{idx, total}] = incl || excl
+        
+        return memo[pair{idx, total}]
     }
-    return dp[partTarget]
+    
+    return dfs(0, target)
 }
