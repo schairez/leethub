5c4b7baccd49ae9b5cb3cef53fe75e72047bcf17
Sergio Chairez <schairezv@gmail.com>
2022-05-04 16:30:00 -0700

    Time: 10 ms (97.22%), Space: 2.4 MB (94.44%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.go
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.go
@@ -1,69 +1,66 @@
-// 416. Partition Equal Subset Sum
-// rolling array DP table
-// time: O(n*target)
-// space: O(target)
 func canPartition(nums []int) bool {
     n := len(nums)
-    sumTotal := 0
+    // sum values
+    sum := 0
     for i := range nums {
-        sumTotal += nums[i]
+        sum += nums[i]
     }
-    if sumTotal % 2 == 1 {
+    // even or odd
+    if sum % 2 == 1 {
         return false
     }
-    target := sumTotal >> 1
+    // time: O(n*target)
+    target := sum >> 1
     dp := make([]bool, target+1)
     dp[0] = true
     for i := 0; i < n; i++ {
         for j := target; j >= nums[i]; j-- {
-            if dp[target] {
+            if dp[target] { 
                 return true
             }
+            //      excl     incl
             dp[j] = dp[j] || dp[j - nums[i]]
         }
     }
     return dp[target]
+    
 }
 
-// top down dfs approach
-// time: O(n*target)
-// space: O(n*target)
+// top down memoized
 func canPartitionMemo(nums []int) bool {
     n := len(nums)
-    sumTotal := 0
+    // sum values
+    sum := 0
     for i := range nums {
-        sumTotal += nums[i]
+        sum += nums[i]
     }
-    if sumTotal % 2 == 1 {
+    // even or odd
+    if sum % 2 == 1 {
         return false
     }
-    type pair struct {idx, total int}
+    target := sum >> 1
+    type pair struct {idx, target int}
     memo := make(map[pair]bool)
-    var (
-        dfs func(int, int) bool
-    )
-    // target is half
-    target := sumTotal >> 1
-    dfs = func(idx, total int) bool {
-        if total == 0 {
+    
+    var dfs func(int, int) bool
+    dfs = func(idx, target int) bool {
+        if target == 0 {
             return true
         }
-        if total < 0 || idx >= n {
+        if target < 0 || idx >= n {
             return false
         }
-        if isPossible, exists := memo[pair{idx, total}]; exists { 
+        if isPossible, exists := memo[pair{idx, target}]; exists {
             return isPossible
         }
         includeItem := false
-        if total - nums[idx] >= 0 {
-            includeItem = dfs(idx+1, total - nums[idx])
+        if target - nums[idx] >= 0 {
+            includeItem = dfs(idx+1, target - nums[idx])
         }
-        // include item or exclude item
-        memo[pair{idx, total}] = includeItem || dfs(idx+1, total)
+        memo[pair{idx, target}] = includeItem || dfs(idx+1, target)
         
-        
-        return memo[pair{idx, total}]
+        return memo[pair{idx, target}]
     }
-    
     return dfs(0, target)
+    
 }
