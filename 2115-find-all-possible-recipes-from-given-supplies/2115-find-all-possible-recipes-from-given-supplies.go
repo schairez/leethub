26649a414321ea7d85905ac09b84963fc05be7ae
Sergio Chairez <schairezv@gmail.com>
2022-05-06 16:11:49 -0700

    Time: 179 ms (72.97%), Space: 8 MB (58.11%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.go
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.go
@@ -1,14 +1,13 @@
 
-
+// Kahn's BFS
 func findAllRecipes(recipes []string, ingredients [][]string, supplies []string) []string {
-    recipeSet := make(map[string]struct{}, len(recipes))
+    n := len(recipes)
+    inDegree := make(map[string]int, n)
+    recipeSet := make(map[string]struct{}, n)
     for i := range recipes {
         recipeSet[recipes[i]] = struct{}{}
     }
     graph := make(map[string][]string)
-    // in degree counter for each node
-    inDegree := make(map[string]int)
-    // O(V+E)
     for i := range recipes {
         inDegree[recipes[i]] += len(ingredients[i])
         for _, ingredient := range ingredients[i] {
@@ -18,36 +17,37 @@ func findAllRecipes(recipes []string, ingredients [][]string, supplies []string)
             graph[ingredient] = append(graph[ingredient], recipes[i])
         }
     }
+    var node string
     queue := make([]string, len(supplies))
+    // indegree = 0 for supplies
     for i := range supplies {
         queue[i] = supplies[i]
     }
-    for nodeKey := range inDegree {
-        if inDegree[nodeKey] == 0 {
-            queue = append(queue, nodeKey)
+    for node := range inDegree {
+        if inDegree[node] == 0 {
+            queue = append(queue, node)
         }
     }
-    res := make([]string, 0, len(recipes))
-    var node string
+    res := make([]string, 0, n)
+    
+    // while not empty
     for len(queue) != 0 {
         for currLen := len(queue); currLen != 0; currLen-- {
+            // dequeue, popleft
             node, queue = queue[0], queue[1:]
             if _, exists := recipeSet[node]; exists {
                 res = append(res, node)
-                delete(recipeSet, node)
             }
-            // decrease indegree of recipe dst node
             neighbors := graph[node]
             if len(neighbors) != 0 {
-                for _, nei := range neighbors {
-                    inDegree[nei]--
-                    if inDegree[nei] == 0 {
-                        queue = append(queue, nei)
+                for _, neiNode := range neighbors {
+                    inDegree[neiNode]--
+                    if inDegree[neiNode] == 0 {
+                        queue = append(queue, neiNode)
                     }
                 }
             }
         }
     }
-    
     return res
 }
