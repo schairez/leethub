b79823bd8e6094d047e3ba0982aa9d962e0cdfb2
Sergio Chairez <schairezv@gmail.com>
2022-05-05 19:02:34 -0700

    Time: 1497 ms (7.14%), Space: 6.4 MB (100.00%) - LeetHub

--- 361-bomb-enemy/361-bomb-enemy.go
+++ 361-bomb-enemy/361-bomb-enemy.go
@@ -1,17 +1,22 @@
+// 361. Bomb Enemy
+// bfs approach
+// time: O(m*n + (m+n))
+// space: O(1)
 
-const (
-    wall  byte = 'W'
-    enemy byte = 'E'
-    empty byte = '0'
-)
 func max(a, b int) int {if a >=b {return  a}; return b}
 
 func maxKilledEnemies(grid [][]byte) int {
+    const (
+        wall  byte = 'W'
+        enemy byte = 'E'
+        empty byte = '0'
+    )
     dirs := [5]int{1,0,-1,0,1}
     numR, numC := len(grid), len(grid[0])
     isValid := func(x, y int) bool {
         return x >= 0 && y >= 0 && y < numC && x < numR
     }
+    // O(m+n)
     bfs := func(x, y int) int {
         numEnemies := 0
         for i := 1; i < 5; i++ {
@@ -26,6 +31,7 @@ func maxKilledEnemies(grid [][]byte) int {
         return numEnemies
     }
     res := 0
+    // O(m*n)
     for r := 0; r < numR; r++ {
         for c := 0; c < numC; c++ {
             if grid[r][c] == empty {
