7e3b5fcd04935f74623cede2b43263691cdcb7bf
Sergio Chairez <schairezv@gmail.com>
2022-05-09 18:41:40 -0700

    Time: 7 ms (33.41%), Space: 2.1 MB (16.24%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.go
@@ -0,0 +1,39 @@
+
+func mySqrt(x int) int {
+    if x <= 0 {
+        return 0
+    }
+    //lo, hi := 1, 1 << 31 -1 
+    lo, hi := 1, x-1
+    for lo + 1 < hi {
+        mid := lo + (hi-lo) >> 1
+        if mid*mid <= x {
+            lo = mid
+        } else {
+            hi = mid
+        }
+    }
+    return lo
+}
+
+//time: O(logN)
+//space:O(1)
+
+func mySqrtV2(x int) int {
+    target := x
+    lo, hi := 1, target
+    for lo < hi { //breaks when lo==hi
+        m := lo + (hi-lo) / 2
+        if m * m < target {
+            lo = m+1
+        } else {
+            hi = m
+        }
+    }
+    if lo * lo > target { 
+        return lo - 1
+    }
+    
+    return lo
+}
+
