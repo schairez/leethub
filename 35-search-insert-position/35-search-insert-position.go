705ccc33cd548d3611b3e7a5267184a3043922ac
Sergio Chairez <schairezv@gmail.com>
2022-05-10 15:54:27 -0700

    Time: 0 ms (100.00%), Space: 2.9 MB (100.00%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.go
+++ 35-search-insert-position/35-search-insert-position.go
@@ -1,4 +1,28 @@
+
 func searchInsert(nums []int, target int) int {
+    n := len(nums)
+    lo, hi := 0, n-1
+    for lo + 1 < hi {
+        mid := lo + (hi - lo) >> 1
+        if nums[mid] <= target {
+            lo = mid
+        } else {
+            hi = mid
+        }
+    }
+    if nums[lo] >= target {
+        return lo
+    }
+    if nums[hi] >= target {
+        return hi
+    }
+    if nums[hi] < target {
+        return hi+1
+    }
+    return lo
+}
+
+func searchInsertV2(nums []int, target int) int {
     n := len(nums)
     lo, hi := 0, n
     for lo < hi {
