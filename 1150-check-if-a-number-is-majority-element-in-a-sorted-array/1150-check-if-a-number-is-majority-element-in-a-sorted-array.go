d42490ae245aeef92e7b8b3ff366957b88d49287
Sergio Chairez <schairezv@gmail.com>
2022-05-10 03:09:01 -0700

    Time: 5 ms (28.57%), Space: 2.6 MB (57.14%) - LeetHub

---
+++ 1150-check-if-a-number-is-majority-element-in-a-sorted-array/1150-check-if-a-number-is-majority-element-in-a-sorted-array.go
@@ -0,0 +1,38 @@
+func isMajorityElement(nums []int, target int) bool {
+    // find leftmost idx of t
+    n := len(nums)
+    l, r := 0, n-1
+    for l + 1 < r {
+        mid := l + (r-l) >> 1
+        if nums[mid] >= target {
+            r = mid
+        } else {
+            l = mid
+        }
+    }
+    var leftMost int
+    if nums[l] == target {
+        leftMost = l
+    } else if nums[r] == target {
+        leftMost = r
+    } else {
+        return false
+    }
+    l, r = 0, n-1
+    for l + 1 < r {
+        mid := l + (r-l) >> 1
+        if nums[mid] <= target {
+            l = mid
+        } else {
+            r = mid
+        }
+    }
+    var rightMost int
+    if nums[r] == target {
+        rightMost = r
+    } else if nums[l] == target {
+        rightMost = l
+    }
+    return (rightMost - leftMost + 1) > n / 2
+    
+}
