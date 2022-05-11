bdad4695469ade65962b12850fbd862608e1b197
Sergio Chairez <schairezv@gmail.com>
2022-05-11 00:36:12 -0700

    Time: 10 ms (56.67%), Space: 4.5 MB (65.65%) - LeetHub

---
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.go
@@ -0,0 +1,16 @@
+func peakIndexInMountainArray(arr []int) int {
+    n := len(arr)
+    lo, hi := 0, n-1 
+    for lo + 1 < hi {
+        mid := lo + (hi-lo) >> 1
+        if arr[mid] > arr[mid+1] {
+            hi = mid
+        } else {
+            lo = mid
+        }
+    }
+    if arr[lo] > arr[lo+1] {
+        return lo
+    }
+    return hi
+}
