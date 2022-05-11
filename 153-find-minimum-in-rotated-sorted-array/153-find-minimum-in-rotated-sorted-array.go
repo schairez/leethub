ec31439e27b16e45d07ffb21ae7b2fb0174e8164
Sergio Chairez <schairezv@gmail.com>
2022-05-10 20:42:02 -0700

    Time: 3 ms (52.25%), Space: 2.5 MB (67.75%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.go
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.go
@@ -1,3 +1,4 @@
+//153. Find Minimum in Rotated Sorted Array
 //time:O(logN)
 //space:O(1)
 
@@ -21,20 +22,3 @@ func findMin(nums []int) int {
     }
     return nums[hi]
 }
-
-func findMinAlt(nums []int) int {
-    var mid int
-    lo, hi := 0, len(nums)-1
-    for nums[lo] > nums[hi] {
-        mid = lo + (hi-lo) >> 1
-        if nums[mid] < nums[hi] {
-            hi = mid
-        } else {
-            lo = mid+1
-        }
-        
-    }
-    return nums[lo]
-    
-}
-
