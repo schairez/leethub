ea8f8d0735235501a28b8318a225ca21af7f81b5
Sergio Chairez <schairezv@gmail.com>
2022-05-13 20:07:43 -0700

    Time: 47 ms (85.42%), Space: 8.3 MB (83.33%) - LeetHub

---
+++ 528-random-pick-with-weight/528-random-pick-with-weight.go
@@ -0,0 +1,43 @@
+
+type Solution struct {
+    totalWt int
+    prefixWtSum []int
+}
+
+
+func Constructor(w []int) Solution {
+    n := len(w)
+    totalWt := w[0]
+    prefixWtSum := w
+    for i := 1; i < n; i++ {
+        totalWt += prefixWtSum[i]
+        prefixWtSum[i] += prefixWtSum[i-1]
+    }
+    return Solution{totalWt, prefixWtSum}
+}
+
+
+func (this *Solution) PickIndex() int {
+    target := rand.Intn(this.totalWt)+1
+    lo, hi := 0, len(this.prefixWtSum)-1
+    // low_bound()
+    for lo + 1 < hi {
+        mid := lo + (hi-lo) >> 1
+        if this.prefixWtSum[mid] >= target {
+            hi = mid
+        } else {
+            lo = mid
+        }
+    }
+    if this.prefixWtSum[lo] >= target {
+        return lo
+    }
+    return hi
+}
+
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * obj := Constructor(w);
+ * param_1 := obj.PickIndex();
+ */
