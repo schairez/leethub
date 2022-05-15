0ee2f5dc6976602a57060abd23bc69ccadbcef54
Sergio Chairez <schairezv@gmail.com>
2022-05-15 00:29:00 -0700

    Time: 56 ms (43.59%), Space: 7.4 MB (54.27%) - LeetHub

---
+++ 658-find-k-closest-elements/658-find-k-closest-elements.go
@@ -0,0 +1,46 @@
+func findClosestElements(arr []int, k int, x int) []int {
+    n := len(arr)
+    if k == n {
+        return arr
+    }
+    left := closestElem(arr, x)
+    right := left
+    for right - left + 1 < k {
+        if left-1 < 0 {
+            right++
+        } else if (right+1 == n) {
+            left--
+        } else {
+            if abs(x - arr[left-1]) <= abs(x - arr[right+1]) {
+                left--
+            } else {
+                right++
+            }
+        }
+    }
+    return arr[left:right+1]
+}
+
+func abs(x int) int {
+    if x < 0 {
+        return -x
+    }
+    return x
+}
+func closestElem(nums []int, target int) int {
+    n := len(nums)
+    lo, hi := 0, n-1
+    for lo+1 < hi {
+        mid := lo + (hi-lo) >> 1
+        if nums[mid] >= target {
+            hi = mid
+        } else {
+            lo = mid
+        }
+    }
+    if nums[lo] == target || 
+    abs(target-nums[lo]) <= abs(target - nums[hi]) {
+        return lo
+    }
+    return hi
+}
