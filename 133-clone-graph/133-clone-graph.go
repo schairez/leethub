ba61f221fde748310d223439201b745ba85f3bde
Sergio Chairez <schairezv@gmail.com>
2022-05-19 15:28:46 -0700

    Time: 0 ms (100.00%), Space: 2.9 MB (75.48%) - LeetHub

--- 133-clone-graph/133-clone-graph.go
+++ 133-clone-graph/133-clone-graph.go
@@ -1,6 +1,3 @@
-
-
-
 /**
  * Definition for a Node.
  * type Node struct {
@@ -9,74 +6,24 @@
  * }
  */
 
-
-
-    
-//time : O(|V| + |E|)
-//space: O(N) b/c of recursion stack and visited ad cloneNodeMap aux D.S.
-
-
-
 func cloneGraph(node *Node) *Node {
     if node == nil {
-        return node
+        return nil
     }
     nodeMap := make(map[*Node]*Node)
     
     var dfs func(*Node) *Node
-    dfs = func(origNode *Node) *Node {
-        if origNode == nil {
-            return nil
-        }
-        if nodeCopy, exists := nodeMap[origNode]; exists {
+    dfs = func(orig *Node) *Node {
+        if nodeCopy, exists := nodeMap[orig]; exists {
             return nodeCopy
         }
-        nodeCopy := &Node{ Val: origNode.Val}
-        nodeMap[origNode] = nodeCopy
-        for _, nei := range origNode.Neighbors {
+        nodeCopy := &Node{Val: orig.Val}
+        nodeMap[orig] = nodeCopy
+        for _, nei := range orig.Neighbors {
             nodeCopy.Neighbors = append(nodeCopy.Neighbors, dfs(nei))
         }
         return nodeCopy
     }
     
-    
-    
     return dfs(node)
 }
-
-
-
-
-
-
-//time : O(|V| + |E|)
-// space: O(|V|)
-
-func cloneGraphBFS(node *Node) *Node {
-    if node == nil {
-        return node
-    }
-    // Node.val is unique for each node.
-    nodeMap := make(map[int]*Node)
-    srcNodeCopy := &Node{Val: node.Val}
-    nodeMap[node.Val] = srcNodeCopy
-    var (
-        origNode *Node
-        queue []*Node
-    )
-    queue = append(queue, node)
-    for len(queue) != 0 {
-        origNode, queue = queue[0], queue[1:]
-        for _, nei := range origNode.Neighbors {
-            if _, seen := nodeMap[nei.Val]; !seen {
-                nodeMap[nei.Val] = &Node{Val: nei.Val}
-                queue = append(queue, nei)
-            }
-            currNodeCopy := nodeMap[origNode.Val]
-            currNodeCopy.Neighbors = append(currNodeCopy.Neighbors, nodeMap[nei.Val])
-        }
-    }
-    return srcNodeCopy
-}
-
-
