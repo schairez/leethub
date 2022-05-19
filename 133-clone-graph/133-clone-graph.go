988e438ffe271cac85b99287b9582ca65079c87f
Sergio Chairez <schairezv@gmail.com>
2022-05-19 15:07:04 -0700

    Time: 4 ms (41.59%), Space: 3 MB (14.66%) - LeetHub

--- 133-clone-graph/133-clone-graph.go
+++ 133-clone-graph/133-clone-graph.go
@@ -10,6 +10,13 @@
  */
 
 
+
+    
+//time : O(|V| + |E|)
+//space: O(N) b/c of recursion stack and visited ad cloneNodeMap aux D.S.
+
+
+
 func cloneGraph(node *Node) *Node {
     if node == nil {
         return node
@@ -38,8 +45,14 @@ func cloneGraph(node *Node) *Node {
 }
 
 
-/*
-func cloneGraph(node *Node) *Node {
+
+
+
+
+//time : O(|V| + |E|)
+// space: O(|V|)
+
+func cloneGraphBFS(node *Node) *Node {
     if node == nil {
         return node
     }
@@ -65,56 +78,5 @@ func cloneGraph(node *Node) *Node {
     }
     return srcNodeCopy
 }
-*/
 
 
-
-
-
-
-
-
-
-
-    
-//time : O(|V| + |E|)
-//space: O(N) b/c of recursion stack and visited ad cloneNodeMap aux D.S.
-/*
-func cloneGraph(node *Node) *Node {
-    if node == nil { return nil }
-    
-    //The number of nodes in the graph is in the range [0, 100].
-    var visited [101]bool
-    var cloneNodeMap [101]*Node  //cloneNodeMap := map[int]*Node{}
-    
-    var dfs func(node *Node)
-    dfs = func(node *Node) {
-        if visited[node.Val] { return }
-        visited[node.Val] = true
-        nodeClone := &Node{Val: node.Val, Neighbors: make([]*Node, 0, len(node.Neighbors))}
-        cloneNodeMap[node.Val] = nodeClone
-        for _, srcNeighbor := range node.Neighbors {
-            srcId := srcNeighbor.Val
-            if !visited[srcId] {
-                dfs(srcNeighbor)
-            }
-            neighborClone := cloneNodeMap[srcId] 
-            nodeClone.Neighbors = append(nodeClone.Neighbors, neighborClone)
-        }
-    }
-    
-    if len(node.Neighbors) == 0 { 
-        dfs(node)
-        return cloneNodeMap[node.Val]
-    }
-    
-    for _, srcNeighbor := range node.Neighbors {
-        id := srcNeighbor.Val
-        if !visited[id] {
-            dfs(srcNeighbor)
-        }
-    }
-    return cloneNodeMap[node.Val]
-    
-}
-    */
