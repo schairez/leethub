6413b80a7b63ffbc9c21cf45b8bcc1c7b2a6106e
Sergio Chairez <schairezv@gmail.com>
2022-05-21 01:50:16 -0700

    Time: 148 ms (96.77%), Space: 7.1 MB (100.00%) - LeetHub

--- 1102-path-with-maximum-minimum-value/1102-path-with-maximum-minimum-value.go
+++ 1102-path-with-maximum-minimum-value/1102-path-with-maximum-minimum-value.go
@@ -1,11 +1,107 @@
+
+func maximumMinimumPath(grid [][]int) int {
+    numR, numC := len(grid), len(grid[0])
+    inArea := func(x, y int) bool {
+        return x >= 0 && x < numR && y >= 0 && y < numC
+    }
+    dirs := [5]int{1, 0, -1, 0, 1}
+    pq := newPQ(func(a, b int) bool {
+        return a > b 
+    })
+    heap.Init(pq)
+    visited := make([][]bool, numR)
+    for x := range visited {
+        visited[x] = make([]bool, numC)
+    }
+    heap.Push(pq, pathData{0, 0, grid[0][0]})
+    visited[0][0] = true
+    
+    res := grid[0][0]
+    
+    for pq.Len() != 0 {
+        curr := heap.Pop(pq).(pathData)
+        res = min(res, curr.score)
+        if curr.x == numR-1 && curr.y == numC-1 {
+            break
+        }
+        for i := 1; i < 5; i++ {
+            newX, newY := curr.x + dirs[i-1], curr.y + dirs[i]
+            if inArea(newX, newY) && !visited[newX][newY] {
+                data := pathData{newX, newY, min(curr.score, grid[newX][newY])}
+                heap.Push(pq, data)
+                visited[newX][newY] = true
+            }
+        }
+    }
+    
+    return res
+}
+
+
+
+type pathData struct {
+    x, y, score int
+}
+
+type PriorityQueue struct {
+    data []pathData
+    lessFn func(i, j int) bool
+}
+func newPQ(lessFn func(i, j int) bool) *PriorityQueue {
+    return &PriorityQueue {
+        lessFn: lessFn,
+    }
+}
+
+func (pq *PriorityQueue) Len() int {
+    return len(pq.data)
+}
+func (pq *PriorityQueue) Less(i, j int) bool {
+    return pq.lessFn(pq.data[i].score, pq.data[j].score)
+}
+func (pq *PriorityQueue) Swap(i, j int) {
+    pq.data[i], pq.data[j] = pq.data[j], pq.data[i]
+}
+func (pq *PriorityQueue) Push(v interface{}) {
+    pq.data = append(pq.data, v.(pathData))
+}
+func (pq *PriorityQueue) Pop() interface{} {
+    n := pq.Len()
+    v := pq.data[n-1]
+    pq.data = pq.data[:n-1]
+    return v
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 func max(a, b int) int { if a >= b{ return a}; return b }
 func min(a, b int) int { if a <= b { return a}; return b}
 
-// todo: 
-// k = max(numR, numC)?
+
+// BFS + DP approach
+// k = max(numR, numC)
 // time: O(m*n*k) 
 // space: O(m*n)
-func maximumMinimumPath(grid [][]int) int {
+func maximumMinimumPathDP(grid [][]int) int {
     dirs := [5]int{1, 0,-1, 0, 1}
     
     numR, numC := len(grid), len(grid[0])
