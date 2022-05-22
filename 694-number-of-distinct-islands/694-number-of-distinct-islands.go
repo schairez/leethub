acb5cfcd3b52a520bc48f91d29be8ef87f82e55c
Sergio Chairez <schairezv@gmail.com>
2022-05-22 02:14:41 -0700

    Time: 257 ms (5.68%), Space: 8.2 MB (28.41%) - LeetHub

--- 694-number-of-distinct-islands/694-number-of-distinct-islands.go
+++ 694-number-of-distinct-islands/694-number-of-distinct-islands.go
@@ -4,17 +4,21 @@ func numDistinctIslands(grid [][]int) int {
     inArea := func(x, y int) bool {
         return x >= 0 && x < numR && y >= 0 && y < numC
     }
+    /*
     visited := make([][]bool, numR)
     for x := range visited {
         visited[x] = make([]bool, numC)
     }
+    */
     distinctIslands := make(map[string]struct{})
     var dfs func(int, int, byte, *strings.Builder)
     dfs = func(x, y int, dir byte, sb *strings.Builder) {
-        if !inArea(x, y) || grid[x][y] != 1 || visited[x][y] {
+        //if !inArea(x, y) || grid[x][y] != 1 || visited[x][y] {
+        if !inArea(x, y) || grid[x][y] != 1 {
             return
         }
-        visited[x][y] = true
+        //visited[x][y] = true
+        grid[x][y] = 0
         sb.WriteByte(dir)
         dfs(x - 1, y, 'N', sb)
         dfs(x + 1, y, 'S', sb)
@@ -25,7 +29,8 @@ func numDistinctIslands(grid [][]int) int {
     
     for x := 0; x < numR; x++ {
         for y := 0; y < numC; y++ {
-            if grid[x][y] == 1 && !visited[x][y] {
+            //if grid[x][y] == 1 && !visited[x][y] {
+            if grid[x][y] == 1 {
                 sb := &strings.Builder{}
                 dfs(x, y, '_', sb)
                 cand := sb.String()
