baa42c6644e60a456b62d806bb20caad2b57880d
Sergio Chairez <schairezv@gmail.com>
2022-05-23 01:48:26 -0700

    Time: 7 ms (48.52%), Space: 4.2 MB (42.61%) - LeetHub

---
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.go
@@ -0,0 +1,32 @@
+func isNumeric(b byte) bool {
+    return b >= '0' && b <= '9'
+}
+
+func evalRPN(tokens []string) int {
+    var stack []int
+    for i := range tokens {
+        if len(tokens[i]) > 1 || isNumeric(tokens[i][0]) { 
+            num, _ := strconv.Atoi(tokens[i])
+            stack = append(stack, num)
+        } else {
+            var res int
+            n := len(stack)
+            num2 := stack[n-1]
+            num1 := stack[n-2]
+            stack = stack[:n-2]
+            oper := tokens[i]
+            switch oper {
+            case "+":
+                res = num1 + num2
+            case "-":
+                res = num1 - num2
+            case "*":
+                res = num1 * num2
+            case "/":
+                res = num1 / num2
+            }
+            stack = append(stack, res)
+        }
+    }
+    return stack[len(stack)-1]
+}
