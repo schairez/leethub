86a0f666f9d13fc6c3b93c85fbb95a41c37c5645
Sergio Chairez <schairezv@gmail.com>
2022-05-27 14:56:19 -0700

    Time: 93 ms (78.41%), Space: 8 MB (30.68%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.go
+++ 729-my-calendar-i/729-my-calendar-i.go
@@ -1,5 +1,19 @@
+
+func min(a, b int) int {if a <= b {return a}; return b}
+func max(a, b int) int {if a >= b {return a}; return b}
+
+func overlaps(node *SegNode, start, end int) bool {
+    return max(node.start, start) < min(node.end, end)
+}
+
+type SegNode struct {
+    start, end int
+    left, right *SegNode
+}
+
+
 type MyCalendar struct {
-    Activity [][]int
+    root *SegNode
 }
 
 
@@ -9,15 +23,39 @@ func Constructor() MyCalendar {
 
 
 func (this *MyCalendar) Book(start int, end int) bool {
-    for _, v := range this.Activity {
-        if v[0] < end && start < v[1] {
-            return false
-        }
+    if this.root == nil {
+        this.root = &SegNode{start:start, end: end}
+        return true
     }
-    this.Activity = append(this.Activity, []int{start, end})
-    return true
+    if overlaps(this.root, start, end) {
+        return false
+    }
+    if this.root.start < start {
+        return this.helper(this.root, this.root.right, start, end)
+    } else {
+        return this.helper(this.root, this.root.left, start, end)
+    }
 }
 
+func (this *MyCalendar) helper(parent, childNode *SegNode, start int, end int) bool {
+    if childNode == nil {
+        node := &SegNode{start: start, end: end}
+        if parent.start < start {
+            parent.right = node
+        } else {
+            parent.left = node
+        }
+        return true
+    }
+    if overlaps(childNode, start, end) {
+        return false
+    }
+    if childNode.start < start {
+        return this.helper(childNode, childNode.right, start, end)
+    } else {
+        return this.helper(childNode, childNode.left, start, end)
+    }
+}
 
 /**
  * Your MyCalendar object will be instantiated and called as such:
