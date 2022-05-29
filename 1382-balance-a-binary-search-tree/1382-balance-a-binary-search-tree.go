6f902548b5895b5fee724ea2b6c19c40f0d89c79
Sergio Chairez <schairezv@gmail.com>
2022-05-29 16:52:51 -0700

    Time: 65 ms (61.67%), Space: 8.3 MB (46.67%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.go
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.go
@@ -1,3 +1,7 @@
+// 1382. Balance a Binary Search Tree
+// time: O(n)
+// space: O(n)
+
 /**
  * Definition for a binary tree node.
  * type TreeNode struct {
@@ -7,34 +11,35 @@
  * }
  */
 
+    
 
-func inorder(root *TreeNode) []*TreeNode {
-    var res []*TreeNode
+func balanceBST(root *TreeNode) *TreeNode {
+    nodes := inorder(root)
+    return arrToBST(nodes)
+}
+
+func inorder(root *TreeNode) []int {
+    var res []int
     var dfs func(node *TreeNode)
     dfs = func(node *TreeNode) {
         if node == nil {
             return
         }
         dfs(node.Left)
-        res = append(res, node)
+        res = append(res, node.Val)
         dfs(node.Right)
     }
     dfs(root)
     return res
 }
-func arrToBST(nodes []*TreeNode) *TreeNode {
+func arrToBST(nodes []int) *TreeNode {
     if len(nodes) == 0 {
         return nil
     }
     mid := len(nodes) >> 1
-    retNode := nodes[mid]
-    retNode.Left = arrToBST(nodes[:mid])
-    retNode.Right = arrToBST(nodes[mid+1:])
-    return retNode
-}
-    
-
-func balanceBST(root *TreeNode) *TreeNode {
-    nodes := inorder(root)
-    return arrToBST(nodes)
+    return &TreeNode{
+        Val: nodes[mid],
+        Left: arrToBST(nodes[:mid]),
+        Right: arrToBST(nodes[mid+1:]),
+    }
 }
