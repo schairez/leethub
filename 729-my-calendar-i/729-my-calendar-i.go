c70cd1559e78ba491f854a6f0ff78ab142bb67fb
Sergio Chairez <schairezv@gmail.com>
2022-05-29 18:07:00 -0700

    Time: 78 ms (88.51%), Space: 8.1 MB (29.89%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.go
+++ 729-my-calendar-i/729-my-calendar-i.go
@@ -1,19 +1,19 @@
+// augmented binary search tree approach
+// time: O(n)
+// space: O(n)
+
+
 
 func min(a, b int) int {if a <= b {return a}; return b}
 func max(a, b int) int {if a >= b {return a}; return b}
 
-func overlaps(node *SegNode, start, end int) bool {
+func overlaps(node *llrbNode, start, end int) bool {
     return max(node.start, start) < min(node.end, end)
-}
-
-type SegNode struct {
-    start, end int
-    left, right *SegNode
 }
 
 
 type MyCalendar struct {
-    root *SegNode
+    root *llrbNode
 }
 
 
@@ -24,38 +24,93 @@ func Constructor() MyCalendar {
 
 func (this *MyCalendar) Book(start int, end int) bool {
     if this.root == nil {
-        this.root = &SegNode{start:start, end: end}
+        this.root = &llrbNode{start:start, end: end}
         return true
     }
-    if overlaps(this.root, start, end) {
+    var canBook bool
+    this.root, canBook = insertHelper(this.root, start, end)
+    if !canBook {
         return false
     }
-    if this.root.start < start {
-        return this.helper(this.root, this.root.right, start, end)
-    } else {
-        return this.helper(this.root, this.root.left, start, end)
-    }
+    return true
 }
 
-func (this *MyCalendar) helper(parent, childNode *SegNode, start int, end int) bool {
-    if childNode == nil {
-        node := &SegNode{start: start, end: end}
-        if parent.start < start {
-            parent.right = node
-        } else {
-            parent.left = node
-        }
-        return true
-    }
-    if overlaps(childNode, start, end) {
+
+
+
+type llrbNode struct {
+    start, end int
+    left, right *llrbNode
+    isRed bool
+}
+func isRedFn(node *llrbNode) bool {
+    if node == nil {
         return false
     }
-    if childNode.start < start {
-        return this.helper(childNode, childNode.right, start, end)
-    } else {
-        return this.helper(childNode, childNode.left, start, end)
-    }
+    return node.isRed
 }
+
+func flipColors(x *llrbNode) {
+    x.isRed = true 
+    x.left.isRed = false
+    x.right.isRed = false
+}
+
+func rotateRight(x *llrbNode) *llrbNode {
+    y := x.left
+    x.left = y.right
+    y.right = x
+    y.isRed = y.right.isRed
+    y.right.isRed = true
+    return y
+}
+
+func rotateLeft(x *llrbNode) *llrbNode {
+    y := x.right
+    x.right = y.left
+    y.left = x
+    y.isRed = y.left.isRed
+    y.left.isRed = true 
+    return y
+}
+
+func insertHelper(node *llrbNode, start int, end int) (*llrbNode, bool) {
+    if node == nil {
+        newChildNode := &llrbNode{start: start, end: end, isRed: true}
+        return newChildNode, true
+    }
+    if overlaps(node, start, end) {
+        return node, false
+    }
+    var canInsert bool
+    if node.start < start {
+        node.right, canInsert = insertHelper(node.right, start, end)
+    } else {
+        node.left, canInsert = insertHelper(node.left, start, end)
+    }
+    if !canInsert {
+        return node, false
+    }
+    // rebalance nodes
+    // tree is right heavy
+    if isRedFn(node.right) && !isRedFn(node.right.right) {
+        node = rotateLeft(node)
+    }
+    // tree is left heavy
+    if isRedFn(node.left) && isRedFn(node.left.left) {
+        node = rotateRight(node)
+    }
+    if isRedFn(node.left) && isRedFn(node.right) {
+        flipColors(node)
+    }
+    return node, true
+}
+
+
+
+
+
+
 
 /**
  * Your MyCalendar object will be instantiated and called as such:
