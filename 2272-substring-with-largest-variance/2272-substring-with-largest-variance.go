6098f0592032b2ad73cdf64b4e3f430753501452
Sergio Chairez <schairezv@gmail.com>
2022-06-10 00:00:11 -0700

    Time: 80 ms (86.67%), Space: 2.3 MB (36.67%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.go
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.go
@@ -3,13 +3,26 @@ func max(a, b int) int { if a >= b { return a}; return b}
 func largestVariance(s string) int {
     res := 0
     n := len(s)
-    for char1 := 'a'; char1 <= 'z'; char1++ {
-        for char2 := 'a'; char2 <= 'z'; char2++ {
+    var freq [26]int 
+    for i := range s {
+        freq[s[i]-'a']++
+    }
+    //fmt.Println(freq)
+    keys := make([]byte, 0, 26)
+    for i := range freq {
+        if freq[i] != 0 {
+            keys = append(keys, byte(i + 'a'))
+        }
+    }
+    fmt.Println(keys)
+    for _, char1 := range keys {
+        for _, char2 := range keys {
             if char1 == char2 {
                 continue
             }
             diff, diffWchar2 := 0, -n
-            for _, char := range s {
+            for i := 0; i < n; i++ {
+                char := s[i]
                 if char == char1 {
                     diff++
                     diffWchar2++
