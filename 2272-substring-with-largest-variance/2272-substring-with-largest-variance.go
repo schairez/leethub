cc1897feb1c653d6af0294fd54e24a1fd6bea2d8
Sergio Chairez <schairezv@gmail.com>
2022-06-09 23:28:24 -0700

    Time: 260 ms (16.67%), Space: 2.3 MB (36.67%) - LeetHub

---
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.go
@@ -0,0 +1,86 @@
+func max(a, b int) int {if a >= b {return a}; return b}
+
+func largestVariance(s string) int {
+    n := len(s)
+    var freq [26]int
+    for i := range s {
+        freq[s[i]-'a']++
+    }
+    f := func(ch1, ch2 byte, isRev bool) int {
+        cnt1, cnt2, i, variance := 0, 0, 0, 0
+        if isRev { 
+            i = n-1 
+        }
+        for i >= 0 && i < n {
+            if s[i] == ch1 {
+                cnt1++
+            } else if s[i] == ch2 {
+                cnt2++
+            }
+            if cnt1 < cnt2 {
+                cnt1 = 0
+                cnt2 = 0
+            }
+            if cnt1 != 0 && cnt2 != 0 {
+                cand := cnt1-cnt2
+                variance = max(variance, cand)
+            }
+            if isRev {
+                i--
+            } else {
+                i++
+            }
+        }
+        return variance
+    }
+    res := 0
+    for ch1 := byte('a'); ch1 <= byte('z'); ch1++ {
+        for ch2 := byte('a'); ch2 <= byte('z'); ch2++ {
+            if ch1 == ch2 || freq[ch1-'a'] == 0 || freq[ch2-'a'] == 0 {
+                continue
+            }
+            res = max(res, f(ch1, ch2, true))
+            res = max(res, f(ch1, ch2, false))
+        }
+    }
+    return res
+}
+
+
+
+
+            
+            /*
+            cnt1, cnt2 := 0, 0
+            for i := 0; i < n; i++ {
+                if s[i] == ch1 {
+                    cnt1++
+                } else if s[i] == ch2 {
+                    cnt2++
+                }
+                if cnt1 < cnt2 {
+                    cnt1 = 0
+                    cnt2 = 0
+                }
+                if cnt1 != 0 && cnt2 != 0 {
+                    cand := cnt1-cnt2
+                    res = max(res, cand)
+                }
+            } 
+            cnt1, cnt2 = 0, 0
+            for i := n-1; i >= 0; i-- {
+                if s[i] == ch1 {
+                    cnt1++
+                } else if s[i] == ch2 {
+                    cnt2++
+                }
+                if cnt1 < cnt2 {
+                    cnt1 = 0
+                    cnt2 = 0
+                }
+                if cnt1 != 0 && cnt2 != 0 {
+                    cand := cnt1-cnt2
+                    res = max(res, cand)
+                }
+            } 
+            */
