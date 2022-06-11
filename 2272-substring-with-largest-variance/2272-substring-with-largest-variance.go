9dd72d5db03c4427ed3f5bbb30b6859847be3a97
Sergio Chairez <schairezv@gmail.com>
2022-06-10 19:52:02 -0700

    Time: 80 ms (86.67%), Space: 2.3 MB (86.67%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.go
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.go
@@ -1,40 +1,44 @@
-func max(a, b int) int { if a >= b { return a}; return b}
-
+func max(a, b int) int {if a >= b {return a}; return b}
 func largestVariance(s string) int {
-    res := 0
     n := len(s)
     var freq [26]int 
+    alphaSet := make(map[byte]struct{}, 26)
+    // is char freq greater than 1
+    isCharFreq := false
     for i := range s {
-        freq[s[i]-'a']++
-    }
-    //fmt.Println(freq)
-    keys := make([]byte, 0, 26)
-    for i := range freq {
-        if freq[i] != 0 {
-            keys = append(keys, byte(i + 'a'))
+        key := int(s[i] - 'a')
+        freq[key]++
+        alphaSet[s[i]] = struct{}{}
+        if !isCharFreq && freq[key] > 1 {
+            isCharFreq = true
         }
     }
-    //fmt.Println(keys)
-    for _, char1 := range keys {
-        for _, char2 := range keys {
+    res := 0
+    if !(isCharFreq && len(alphaSet) > 1) {
+        return res
+    }
+    for char1 := range alphaSet {
+        for char2 := range alphaSet {
             if char1 == char2 {
                 continue
-            }
-            diff, diffWchar2 := 0, -n
-            for i := 0; i < n; i++ { 
-                char := s[i]
-                if char == char1 {
-                    diff++
-                    diffWchar2++
-                } else if char == char2 {
-                    diff--
-                    diffWchar2 = diff 
-                    diff = max(diff, 0)
+            } 
+            cnt1, cnt2, rem2 := 0, 0, freq[char2-'a']
+            for i := 0; i < n; i++ {
+                curr := s[i]
+                if curr == char1 {
+                    cnt1++
+                } else if curr == char2 {
+                    cnt2++
+                    rem2--
                 }
-                res = max(res, diffWchar2)
+                if cnt1 < cnt2 && rem2 > 0 {
+                    cnt1, cnt2 = 0, 0
+                }
+                if cnt1 != 0 && cnt2 != 0 {
+                    res = max(res, cnt1 - cnt2)
+                }
             }
         }
     }
     return res
-    
 }
