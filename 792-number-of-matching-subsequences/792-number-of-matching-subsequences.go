7fdb6a5fc6b25ae3311aa24d47e08cf612462f39
Sergio Chairez <schairezv@gmail.com>
2022-06-20 01:13:49 -0700

    Time: 87 ms (88.24%), Space: 9.5 MB (16.18%) - LeetHub

---
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.go
@@ -0,0 +1,47 @@
+func numMatchingSubseq(s string, words []string) int {
+    var charIdxMap [26][]int
+    for idx := range s {
+        charIdxMap[s[idx]-'a'] = append(charIdxMap[s[idx]-'a'], idx)
+    }
+    res := 0
+    for _, word := range words {
+        currIdx := 0
+        for wIdx := range word {
+            arr := charIdxMap[word[wIdx]-'a']
+            if n := len(arr); n == 0 || arr[n-1] < currIdx {
+                currIdx = -1
+                break
+            }
+            currIdx = binSearch(arr, currIdx)
+            currIdx++
+        }
+        if currIdx != -1 {
+            res++
+        }
+    }
+    return res
+}
+
+
+// invariant: return first val >= t if exists
+func binSearch(arr []int, target int) int {
+    n := len(arr)
+    if n == 0 {
+        return -1
+    }
+    lo, hi := 0, n-1
+    for lo+1 < hi {
+        mid := lo + (hi-lo) >> 1
+        if arr[mid] >= target {
+            hi = mid
+        } else {
+            lo = mid
+        }
+    }
+    if arr[lo] >= target {
+        return arr[lo]
+    } else if arr[hi] >= target {
+        return arr[hi]
+    }
+    return -1
+}
