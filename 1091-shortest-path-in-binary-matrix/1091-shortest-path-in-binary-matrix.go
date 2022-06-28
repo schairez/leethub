f9fcb106d0a7c297a972392194d96692735d3258
Sergio Chairez <schairezv@gmail.com>
2022-06-28 03:09:29 -0700

    Time: 92 ms (37.27%), Space: 8.7 MB (12.95%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.go
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.go
@@ -1,9 +1,8 @@
-// time: O(m*n)
-// space: O(m*n)
 func shortestPathBinaryMatrix(grid [][]int) int {
     if grid[0][0] == 1 {
         return -1
     }
+    numR, numC := len(grid), len(grid[0])
     dirs := [8][2]int{
         {-1,0}, // N
          {1,0}, // S
@@ -14,44 +13,38 @@ func shortestPathBinaryMatrix(grid [][]int) int {
          {1,1},  // SW
          {1,-1}, // NE
     }
-    numR, numC := len(grid), len(grid[0])
-    // hashset
+    var (
+        queue [][2]int
+        currNode [2]int
+    )
     visited := make([][]bool, numR)
     for x := range visited {
         visited[x] = make([]bool, numC)
     }
-    srcNode := [2]int{0,0}
-    x, y := srcNode[0], srcNode[1]
-    var (
-        queue [][2]int  
-        currNode [2]int
-        dist     int
-    )
-    queue = append(queue, srcNode)
-    visited[x][y] = true
+    visited[0][0] = true
+    queue = append(queue, [2]int{0,0})
+    shortestDist := 1
     for len(queue) != 0 {
-       dist++ 
         for currLen := len(queue); currLen != 0; currLen-- {
             currNode, queue = queue[0], queue[1:]
             x, y := currNode[0], currNode[1]
-            if x == numR -1 && y == numC -1 {
-                return dist
+            if x == numR-1 && y == numC-1 {
+                return shortestDist
             }
-            for i := 0; i < 8; i++ {
-                newX, newY := x + dirs[i][0], y + dirs[i][1]
-                if (newX < 0 || newX >= numR || newY < 0 || newY >= numC) {
+            for _, dir := range dirs {
+                dX, dY := dir[0], dir[1]
+                newX, newY := x + dX, y + dY 
+                if newX < 0 || newX >= numR || newY < 0 || newY >= numC {
                     continue
                 }
-                if grid[newX][newY] == 1 {
+                if grid[newX][newY] == 1 || visited[newX][newY] {
                     continue
                 }
-                if visited[newX][newY] {
-                    continue
-                }
-                queue = append(queue, [2]int{newX, newY})
                 visited[newX][newY] = true
+                queue = append(queue, [2]int{newX, newY})
             }
         }
+        shortestDist++
     }
     return -1
 }
