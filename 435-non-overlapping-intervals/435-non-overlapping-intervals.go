40089c51cb049311982f6ef9a7014680cc4cf2b9
Sergio Chairez <schairezv@gmail.com>
2022-07-05 16:49:58 -0700

    Time: 345 ms (43.64%), Space: 16.4 MB (81.82%) - LeetHub

---
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.go
@@ -0,0 +1,24 @@
+
+func max(a, b int) int { if a >= b {return a}; return b}
+func min(a, b int) int { if a <= b {return a}; return b}
+
+func isOverlap(interval1, interval2 []int) bool {
+    return max(interval1[0], interval2[0]) < min(interval1[1], interval2[1])
+}
+
+func eraseOverlapIntervals(intervals [][]int) int {
+    n := len(intervals)
+    res := 0
+    sort.Slice(intervals, func(i, j int) bool {
+        return intervals[i][1] < intervals[j][1]
+    })
+    prev := intervals[0]
+    for i := 1; i < n; i++ {
+        if isOverlap(prev, intervals[i]) {
+            res++
+            continue
+        }
+        prev = intervals[i]
+    }
+    return res
+}
